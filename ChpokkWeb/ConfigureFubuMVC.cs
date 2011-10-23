68f60892481773408c86f0164f5f1c6d885f0dbd
Artem Smirnov <uluhonolulu@gmail.com>
2011-10-23 10:23:09 +0400

    Started creating the demo view and route

--- ChpokkWeb/ConfigureFubuMVC.cs
+++ ChpokkWeb/ConfigureFubuMVC.cs
@@ -1,4 +1,14 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using ChpokkWeb.Shared;
 using FubuMVC.Core;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.Routes;
+using FubuMVC.Core.View;
+using FubuMVC.Spark;
+using FubuMVC.Spark.SparkModel;
+using StructureMap;
 
 namespace ChpokkWeb
 {
@@ -21,9 +31,42 @@ namespace ChpokkWeb
 
 			//Output.ToJson.WhenCallMatches(action => action.HasAttribute<JsonEndpointAttribute>());
 
+			((FubuRegistry)this).UseSpark();
+
             // Match views to action methods by matching
             // on model type, view name, and namespace
-            Views.TryToAttachWithDefaultConventions();
+			//ITemplateRegistry registry = null;
+			//this.Services(reg =>
+			//                {
+			//                    registry = reg.DefaultServiceFor<ITemplateRegistry>().Value as ITemplateRegistry;
+			//                });
+			Views
+				//.TryToAttachWithDefaultConventions()
+				.RegisterActionLessViews(
+				token => {
+					return token.ViewModelType == typeof(DummyModel);
+				}, chain =>
+				   	{
+				   		chain.Route = new RouteDefinition("demostuff");
+				   	})
+					//.Facility(new ModellessSparkViewFacility(registry))
+				;
         }
     }
+
+	public class ModellessSparkViewFacility : IViewFacility {
+		private readonly ITemplateRegistry _templateRegistry;
+		public ModellessSparkViewFacility(ITemplateRegistry templateRegistry) {
+			_templateRegistry = templateRegistry;
+		}
+
+		public IEnumerable<IViewToken> FindViews(TypePool types, BehaviorGraph graph) {
+			var descriptors = from template in _templateRegistry.AllTemplates()
+			                  where template.Descriptor is ViewDescriptor
+			                  select template.Descriptor as ViewDescriptor;
+			var tokens = from descriptor in descriptors where !descriptor.HasViewModel() select new SparkViewToken(descriptor);
+			return tokens.Cast<IViewToken>();
+
+		}
+	}
 }
