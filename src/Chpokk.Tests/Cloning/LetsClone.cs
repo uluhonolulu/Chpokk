1e2e771bcd1285533ac2359c8f8b201a57b34986
Artem Smirnov <uluhonolulu@gmail.com>
2012-03-05 23:20:54 +0400

    Now I know how to commit stuff to GitHub

---
+++ src/Chpokk.Tests/Cloning/LetsClone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace Chpokk.Tests.Cloning {
+	[TestFixture]
+	public class LetsClone {
+		[Test]
+		public void Test() {
+			//
+			// TODO: Add test logic here
+			//
+		}
+	}
+}
