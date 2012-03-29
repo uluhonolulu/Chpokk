4aec6be0e40273efa3aa55d1bd30a1557c8da508
Artem Smirnov <uluhonolulu@gmail.com>
2012-03-29 11:14:39 +0400

    One more attempt with the Spark problem (solved!) Test file deleted by a test

--- src/Chpokk.Tests/Exploring/ICanSeeIt.cs
+++ src/Chpokk.Tests/Exploring/ICanSeeIt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using CThru;
 using CThru.BuiltInAspects;
@@ -8,6 +9,7 @@ using Ivonna.Framework;
 using MbUnit.Framework;
 using MbUnit.Framework.ContractVerifiers;
 using Ivonna.Framework.Generic;
+using System.Linq;
 
 namespace Chpokk.Tests.Exploring {
 	[TestFixture, RunOnWeb]
@@ -16,9 +18,38 @@ namespace Chpokk.Tests.Exploring {
 		public void DoesntThrow() {
 			//CThruEngine.AddAspect(new TraceAspect(info => info.TypeName.EndsWith("CompilerError"), 5));
 			//CThruEngine.AddAspect(new TraceAspect(info => info.MethodName.EndsWith("ReferencedAssemblies"), 5));
-			CThruEngine.AddAspect(new DebugAspect(info => info.MethodName.EndsWith("CompileAssemblyFromSource")));
+			AppDomain.CurrentDomain.AssemblyLoad += new AssemblyLoadEventHandler(CurrentDomain_AssemblyLoad);
+			CThruEngine.AddAspect(new LetsSeeMyAssemblies(info => info.MethodName.EndsWith("CompileAssemblyFromSource")));
 			CThruEngine.AddAspect(new TraceAspect(info => info.TypeName == "Microsoft.CSharp.CSharpCodeGenerator" && info.MethodName == "Compile", 5));
 			Assert.DoesNotThrow(() => new TestSession().Get("/Main"));  
+		}
+
+		private Assembly CurrentDomainOnAssemblyResolve(object sender, ResolveEventArgs args) {
+			if (args.Name == "Configuration") {
+				var asses = AppDomain.CurrentDomain.GetAssemblies();
+				var confies = asses.Where(ass => ass.GetName().Name == "Configuration").ToArray();
+				Console.WriteLine(confies.Count());
+			}
+			return null;
+		}
+
+		void CurrentDomain_AssemblyLoad(object sender, AssemblyLoadEventArgs args) {
+			if (args.LoadedAssembly.GetName().Name == "Configuration") {
+				var asses = AppDomain.CurrentDomain.GetAssemblies();
+				var confies = asses.Where(ass => ass.GetName().Name == "Configuration").ToArray();
+				Console.WriteLine(confies.Count());
+				//AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+			}
+		}
+	}
+
+	class LetsSeeMyAssemblies : CommonAspect {
+		public LetsSeeMyAssemblies(Predicate<InterceptInfo> shouldIntercept) : base(shouldIntercept) {}
+
+		public override void MethodBehavior(DuringCallbackEventArgs e) {
+			var asses = AppDomain.CurrentDomain.GetAssemblies();
+			var confies = asses.Where(ass => ass.GetName().Name == "Configuration").ToArray();
+			base.MethodBehavior(e);
 		}
 	}
 }
