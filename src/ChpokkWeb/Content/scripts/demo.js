822496a72278afc207372d0294b440bf8f9b4902
Artem Smirnov <uluhonolulu@gmail.com>
2012-04-16 13:15:16 +0400

    More or less done with Suggest validation

--- src/ChpokkWeb/Content/scripts/demo.js
+++ src/ChpokkWeb/Content/scripts/demo.js
@@ -2,15 +2,13 @@
 $(function () {
 	amplify.subscribe("suggestion", function (data) {
 		$.post('/demo/send', data, function (response) {
-			//alert(response.StatusCode);
 			if (response.StatusCode == 200) {
-				$('#suggestDialog').modal('hide');
-				$('#thanks_for_suggestion').dialog('open');
+				suggestionPublished();
 			} else {
-				$('#suggestionError').show();
-				$('#suggestionErrorText').text(response.Message);
+				suggestionError(response.Message);
 			}
 
 		});
 	})
 });
+
