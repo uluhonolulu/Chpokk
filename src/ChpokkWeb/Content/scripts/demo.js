7618e74650c8406e46823286e8205750a9dc9a6d
Artem Smirnov <uluhonolulu@gmail.com>
2012-04-16 11:22:08 +0400

    Error handling in Suggestion dialog.

--- src/ChpokkWeb/Content/scripts/demo.js
+++ src/ChpokkWeb/Content/scripts/demo.js
@@ -2,9 +2,15 @@
 $(function () {
 	amplify.subscribe("suggestion", function (data) {
 		$.post('/demo/send', data, function (response) {
-			alert(response.StatusCode);
-			if (response.StatusCode == 200)
+			//alert(response.StatusCode);
+			if (response.StatusCode == 200) {
+				$('#suggestDialog').modal('hide');
 				$('#thanks_for_suggestion').dialog('open');
+			} else {
+				$('#suggestionError').show();
+				$('#suggestionErrorText').text(response.Message);
+			}
+
 		});
 	})
 });
