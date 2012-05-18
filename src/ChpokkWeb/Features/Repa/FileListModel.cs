89fb9ea7c9332408983e30b1de966b458f8f2abb
Artem Smirnov <uluhonolulu@gmail.com>
2012-05-18 12:10:26 +0400

    Can see the file list and file content.. again!

--- src/ChpokkWeb/Features/Repa/FileListModel.cs
+++ src/ChpokkWeb/Features/Repa/FileListModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ChpokkWeb.Features.Repa {
+﻿namespace ChpokkWeb.Features.Repa {
 	public class FileListModel {
 		public RepositoryItem[] Items { get; set; }
 	}
