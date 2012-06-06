1b6b8c218b334c6217fdb4270af28ee3e36e92a7
Artem Smirnov <uluhonolulu@gmail.com>
2012-06-06 14:50:25 +0400

    Displaying wait icon when cloning

--- src/ChpokkWeb/Content/scripts/common.js
+++ src/ChpokkWeb/Content/scripts/common.js
@@ -2,5 +2,6 @@
 $(function () {
 	amplify.subscribe('ContinuationError', function (continuation) {
 		$('#errorContainer').html(continuation.errors[0].message);
+		$('.waitContainer').hide();
 	});
 });
