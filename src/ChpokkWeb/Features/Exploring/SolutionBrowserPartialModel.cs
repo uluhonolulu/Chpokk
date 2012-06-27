9acc9b476a6aeaf9dcef088374dfb8da53816a13
Artem Smirnov <uluhonolulu@gmail.com>
2012-06-27 12:00:57 +0400

    Created a partial for solution browser, added it to the Project view, hid the file browser for now.

---
+++ src/ChpokkWeb/Features/Exploring/SolutionBrowserPartialModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChpokkWeb.Shared;
+
+namespace ChpokkWeb.Features.Exploring {
+	public class SolutionBrowserPartialModel : IDontNeedActionsModel {
+		public string Name { get; set; }
+	}
+}
