f03827eee768519f5d5b9713d44a69ac224b5aad
Artem Smirnov <uluhonolulu@gmail.com>
2012-06-30 18:52:57 +0400

    Displaying the file path correctly

--- src/Chpokk.Tests/Exploring/ParsingProjectFile.cs
+++ src/Chpokk.Tests/Exploring/ParsingProjectFile.cs
@@ -17,11 +17,27 @@ namespace Chpokk.Tests.Exploring {
 			Assert.AreEqual(1, Result.Count());
 		}
 
+		[Test, DependsOn("CanSeeTheFile")]
+		public void CaptionIsFilename() {
+			Assert.AreEqual(Context.FILE_NAME, FileItem.Name);
+		}
+
+		[Test, DependsOn("CanSeeTheFile")]
+		public void CanEditTheFile() {
+			var expectedFileName = FileSystem.Combine(@"src\ProjectName\Class1.cs");
+			Assert.AreEqual(expectedFileName, FileItem.PathRelativeToRepositoryRoot);
+			Assert.AreEqual("file", FileItem.Type);
+		}
+
+		protected RepositoryItem FileItem {
+			get { return Result.First(); }
+		}
+
 		public override IEnumerable<RepositoryItem> Act() {
 			var loader = Context.Container.Get<SolutionFileLoader>();
 			return
 				loader.CreateProjectItem(Context.SolutionFolder,
-				                         new ProjectItem {Name = Context.PROJECT_NAME, Path = Context.PROJECT_PATH}).Children;
+				                         new ProjectItem {Name = Context.PROJECT_NAME, Path = Context.PROJECT_PATH}, Context.RepositoryRoot).Children;
 		}
 	}
 
--- src/ChpokkWeb/Features/Exploring/SolutionFileLoader.cs
+++ src/ChpokkWeb/Features/Exploring/SolutionFileLoader.cs
@@ -28,23 +28,23 @@ namespace ChpokkWeb.Features.Exploring {
 			};
 			var content = _fileSystem.ReadStringFromFile(filePath);
 			var solutionFolder = filePath.ParentDirectory();
-			var projectItems = _solutionParser.GetProjectItems(content, filePath).Select(item => CreateProjectItem(solutionFolder, item)); 
+			var projectItems = _solutionParser.GetProjectItems(content, filePath).Select(item => CreateProjectItem(solutionFolder, item, repositoryRoot)); 
 			solutionItem.Children.AddRange(projectItems);
 
 			return solutionItem;			
 		}
 
 
-		public RepositoryItem CreateProjectItem(string solutionFolder, ProjectItem projectItem) {
+		public RepositoryItem CreateProjectItem(string solutionFolder, ProjectItem projectItem, string repositoryRoot) {
 			var projectFilePath = FileSystem.Combine(solutionFolder,
 													 projectItem.Path);
 			var projectRepositoryItem = new RepositoryItem() {
 				Name = projectItem.Name,
-				PathRelativeToRepositoryRoot = projectFilePath,
 				Type = "folder"
 			};
 			var projectFileContent = _fileSystem.ReadStringFromFile(projectFilePath);
-			var fileItems = _projectParser.GetCompiledFiles(projectFileContent).Select(item => new RepositoryItem());
+			var projectFolderRelativeToRepositoryRoot = projectFilePath.ParentDirectory().PathRelativeTo(repositoryRoot);
+			var fileItems = _projectParser.GetCompiledFiles(projectFileContent).Select(item => new RepositoryItem{Name = item.Path, PathRelativeToRepositoryRoot = FileSystem.Combine(projectFolderRelativeToRepositoryRoot, item.Path), Type = "file"});
 			projectRepositoryItem.Children.AddRange(fileItems);
 			return projectRepositoryItem;
 		}
