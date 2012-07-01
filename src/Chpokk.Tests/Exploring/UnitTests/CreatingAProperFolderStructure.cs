30cc2317d9f6396ed7689f0e2cfc9bdf5d29e68f
Artem Smirnov <uluhonolulu@gmail.com>
2012-07-01 12:07:55 +0400

    Added support for multiple files in subfolders

--- src/Chpokk.Tests/Exploring/UnitTests/CreatingAProperFolderStructure.cs
+++ src/Chpokk.Tests/Exploring/UnitTests/CreatingAProperFolderStructure.cs
@@ -13,6 +13,7 @@ namespace Chpokk.Tests.Exploring.UnitTests {
 		public void RootFileReturnsSingleFileItem() {
 			var source = new[] {new FileItem {Path = "Class1"}};
 			var result = _converter.Convert(source, "root");
+			Assert.AreEqual(1, result.Count());
 			var item = result.Single();
 
 			Assert.AreEqual("Class1", item.Name);
@@ -22,15 +23,26 @@ namespace Chpokk.Tests.Exploring.UnitTests {
 
 		[Test]
 		public void FileInASubfolderReturnsSubfolderAndFileAsAChild() {
-			var source = new[] {new FileItem {Path = @"Subfolder\\Class1"}};
+			var source = new[] {new FileItem {Path = @"Subfolder\Class1"}};
 			var result = _converter.Convert(source, "root");
 			Assert.AreEqual(1, result.Count());
-			//var item = result.Single();
+			var folderItem = result.Single();
 
-			//Assert.AreEqual("Class1", item.Name);
-			//Assert.AreEqual(@"root\Class1", item.PathRelativeToRepositoryRoot);
-			//Assert.AreEqual("file", item.Type);
-			
+			Assert.AreEqual("Subfolder", folderItem.Name);
+			Assert.AreEqual(@"root\Subfolder", folderItem.PathRelativeToRepositoryRoot);
+			Assert.AreEqual("folder", folderItem.Type);
+			Assert.AreEqual(1, folderItem.Children.Count);
+
+		}
+
+		[Test]
+		public void TwoFilesInTwoSubfolders() {
+			var source = new[] {new FileItem {Path = @"Subfolder1\Class1"}, new FileItem {Path = @"Subfolder2\Class2"}};
+			var result = _converter.Convert(source, "root");
+			Assert.AreEqual(2, result.Count());
+
+			var folderItem1 = result.First();
+			Assert.AreEqual(1, folderItem1.Children.Count);
 		}
 	}
 }
--- src/ChpokkWeb/Features/Exploring/FileItemToProjectItemConverter.cs
+++ src/ChpokkWeb/Features/Exploring/FileItemToProjectItemConverter.cs
@@ -8,7 +8,23 @@ using FubuCore;
 namespace Chpokk.Tests.Exploring {
 	public class FileItemToProjectItemConverter {
 		public IEnumerable<RepositoryItem> Convert(IEnumerable<FileItem> files, string projectFolderRelativeToRepositoryRoot) {
-			return files.Select(item => new RepositoryItem { Name = item.Path, PathRelativeToRepositoryRoot = FileSystem.Combine(projectFolderRelativeToRepositoryRoot, item.Path), Type = "file" });
-		} 
+			var folders = files.Select(item => item.Path.ParentDirectory());
+			var folderItems =
+				folders.Where(folder => folder.IsNotEmpty()).Select(folder => CreateFolderItem(folder, files, projectFolderRelativeToRepositoryRoot));
+			var fileItems = files.Where(item => item.Path.ParentDirectory().IsEmpty()).Select(item => CreateFileItem(item, projectFolderRelativeToRepositoryRoot));
+			return folderItems.Concat(fileItems);
+		}
+
+		private RepositoryItem CreateFileItem(FileItem item, string projectFolderRelativeToRepositoryRoot) {
+			return new RepositoryItem {Name = item.Path, PathRelativeToRepositoryRoot = FileSystem.Combine(projectFolderRelativeToRepositoryRoot, item.Path), Type = "file"};
+		}
+
+		private RepositoryItem CreateFolderItem(string folder, IEnumerable<FileItem> allFiles, string projectFolderRelativeToRepositoryRoot) {
+			var folderItem = new RepositoryItem {Name = folder, PathRelativeToRepositoryRoot = FileSystem.Combine(projectFolderRelativeToRepositoryRoot, folder), Type = "folder"};
+			foreach (var fileItem in allFiles.Where(item => item.Path.StartsWith(folder))) {
+				folderItem.Children.Add(CreateFileItem(fileItem, projectFolderRelativeToRepositoryRoot));
+			}
+			return folderItem;
+		}
 	}
 }
