ce1cec0b5091cfc7fc1574a694f6ae56259523fb
Artem Smirnov <uluhonolulu@gmail.com>
2012-07-02 23:19:56 +0400

    browsing the bin folder

---
+++ src/ChpokkWeb/Features/DirChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChpokkWeb.Features.Exploring;
+using FubuCore;
+
+namespace ChpokkWeb.Features {
+	public class DirCheckerController {
+		private IFileSystem _fileSystem;
+		public DirCheckerController(IFileSystem fileSystem) {
+			_fileSystem = fileSystem;
+		}
+
+		public string Gimme(FileContentInputModel model) {
+			return _fileSystem.FindFiles(FileSystem.Combine(model.PhysicalApplicationPath, "bin"), FileSet.Everything()).Join(Environment.NewLine); 
+		}
+	}
+}
