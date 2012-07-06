74eb6d2c47853a3728604390e5ea6740a613cc5e
Artem Smirnov <uluhonolulu@gmail.com>
2012-07-06 22:18:24 +0400

    Fixed the lib scripts a bit

--- src/ChpokkWeb/Content/scripts/common.js
+++ src/ChpokkWeb/Content/scripts/common.js
@@ -1,7 +1,12 @@
 ﻿
 $(function () {
 	amplify.subscribe('ContinuationError', function (continuation) {
+		debugger;
 		$('#errorContainer').html(continuation.errors[0].message);
 		$('.waitContainer').hide();
 	});
+	$.continuations.bind('AjaxStarted', function () {
+		alert('ajax!');
+	});
+
 });
--- src/ChpokkWeb/Content/scripts/lib/jquery.continuations.amplify.js
+++ src/ChpokkWeb/Content/scripts/lib/jquery.continuations.amplify.js
@@ -7,22 +7,24 @@
 
 (function (continuations) {
 
-    var payloadPolicy = function () {
-        this.matches = function (continuation) {
-            return continuation.topic != null && continuation.payload != null;
-        };
-        this.execute = function (continuation) {
-            amplify.publish(continuation.topic, continuation.payload);
-        };
-    };
-    
-    continuations.applyPolicy(new payloadPolicy());
+	var payloadPolicy = function () {
+		this.matches = function (continuation) {
+			return continuation.topic != null && continuation.payload != null;
+		};
+		this.execute = function (continuation) {
+			amplify.publish(continuation.topic, continuation.payload);
+		};
+	};
 
-    var topics = ['AjaxStarted', 'AjaxCompleted', 'ContinuationError'];
-    for(var i = 0; i < topics.length; i++) {
-        var topic = topics[i];
-        continuations.bind(topic, function(payload) {
-            amplify.publish(topic, payload);
-        });
-    }
-}(jQuery.continuations));
+	continuations.applyPolicy(new payloadPolicy());
+
+	var topics = ['AjaxStarted', 'AjaxCompleted', 'ContinuationError'];
+	for (var i = 0; i < topics.length; i++) {
+		var topic = topics[i];
+		continuations.bind(topic, (function (topicScoped) {
+			return function(payload) {
+				amplify.publish(topicScoped, payload);
+			};
+		})(topic));
+	}
+} (jQuery.continuations));
