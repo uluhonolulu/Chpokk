d21124f625977d59705282474e0a6845bec8b5c0
Artem Smirnov <uluhonolulu@gmail.com>
2012-07-08 19:58:17 +0400

    Intellisense spike works!

--- src/Chpokk.Tests/Spikes/IntelSpike.cs
+++ src/Chpokk.Tests/Spikes/IntelSpike.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Gallio.Framework;
 using ICSharpCode.NRefactory;
 using ICSharpCode.SharpDevelop.Dom;
+using ICSharpCode.SharpDevelop.Dom.CSharp;
 using ICSharpCode.SharpDevelop.Dom.NRefactoryResolver;
 using MbUnit.Framework;
 using MbUnit.Framework.ContractVerifiers;
 using IParser = ICSharpCode.SharpDevelop.Project.IParser;
+using FubuCore;
 
 namespace Chpokk.Tests.Spikes {
 	[TestFixture]
@@ -35,10 +38,11 @@ namespace Chpokk.Tests.Spikes {
 			}
 
 			projectContent.UpdateCompilationUnit(null, compilationUnit, "fakefile1.cs");
-			//var parseInformation = new ParseInformation(compilationUnit);
-			//Console.WriteLine(parseInformation.CompilationUnit.Classes.Count);
+
+	
 			var source2 = @"public class C {
 								void D(){
+									new A().B();
 								}
 							}";
 
@@ -53,12 +57,35 @@ namespace Chpokk.Tests.Spikes {
 			
 
 			Assert.AreEqual(2, projectContent.Classes.Count);
+
+			var parseInformation = new ParseInformation(compilationUnit);
+			var text = @"public class C {
+								void D(){
+									new A().
+								}
+							}";
+
+			var expression = FindExpression(text, text.IndexOf('.'), parseInformation);
+			var resolver = new NRefactoryResolver(LanguageProperties.CSharp);
+			var result = resolver.Resolve(expression, parseInformation, text);
+			Assert.IsNotNull(result);
+			Assert.Equals("A", result.ResolvedType.Name);
+			Console.WriteLine(result.ResolvedType.Name);
+			var completionData = result.GetCompletionData(projectContent);
+			completionData.Cast<IMember>().Each(member => Console.WriteLine(member.Name));
+			Console.WriteLine(result);
 		}
 
 		ICompilationUnit ConvertCompilationUnit(ICSharpCode.NRefactory.Ast.CompilationUnit cu, IProjectContent projectContent) {
 			var converter = new NRefactoryASTConvertVisitor(projectContent, SupportedLanguage.CSharp);
 			cu.AcceptVisitor(converter, null);
 			return converter.Cu;
+		}
+
+		private ExpressionResult FindExpression(string text, int offset, ParseInformation parseInformation) {
+			var finder = new CSharpExpressionFinder(parseInformation);
+			var expression = finder.FindExpression(text, offset);
+			return expression;
 		}
 	}
 }
