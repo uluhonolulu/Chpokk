21e16ac7cb55fcf31e0fd62823b592037eceb1bf
Artem Smirnov <uluhonolulu@gmail.com>
2012-07-14 14:35:01 +0400

    Now code appears in the _editor partial

---
+++ src/ChpokkWeb/Features/Editor/CodeEditorModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChpokkWeb.Features.Editor {
+	public class CodeEditorModel {
+		public string Content { get; set; }
+	}
+}
