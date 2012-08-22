a55a32019bda8ddf59c9fe2e3bc0c238a298a922
Artem Smirnov <uluhonolulu@gmail.com>
2012-08-22 22:52:00 +0400

    Started to implement intellisense properly

--- src/ChpokkWeb/Content/scripts/editor.js
+++ src/ChpokkWeb/Content/scripts/editor.js
@@ -9,16 +9,44 @@ function initEditor() {
 //    $('#code').keyup(function () {
 //        colorize(this);
     //    });
-    var spaceCode = ' '.charCodeAt(0);
-    $.fn.keyz.keymap.space = spaceCode;
+    //var spaceCode = ' '.charCodeAt(0);
+    $.fn.keyz.keymap.space = $.ui.keyCode.SPACE;
     $('#code').keyz(null, null, {
         'space': function (ctl, sft, alt, event) {
-            //debugger;
             colorize(this);
             return true;
+        },
+
+        '.': function () {
+            displayIntellisense(this);
+            return true;
+            function displayIntellisense(editor) {
+                var intelData = {
+                    container: $('#intel_results'),
+                    editor: $('#code'),
+                    show: function () {
+                        //container.hide();
+                        var text = this.editor.text();
+                        //var position = this.editor.caret().start;
+                        var position = text.indexOf('""') + 2;
+                        var intelUrl = 'url::ChpokkWeb.Features.Editor.Intellisense.IntelInputModel';
+                        $.post(intelUrl, { Text: text, Position: position, NewChar: '.', RepositoryName: 'Chpokk-SampleSol', ProjectPath: 'src\\ConsoleApplication1\\ConsoleApplication1.csproj' }, function (inteldata) {
+                            if (inteldata != null && inteldata.Items != null && inteldata.Items.length > 0) {
+                                alert("it works!");
+                            }
+
+                        });
+
+                    }
+                };
+                intelData.show();
+            }
+
         }
     });
 }
+
+
 
 function colorize(editor) {
         var selection = window.getSelection();
