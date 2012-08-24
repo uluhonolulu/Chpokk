5b71226cfd78de9933eb94a3f0180d767e335cac
Artem Smirnov <uluhonolulu@gmail.com>
2012-08-24 23:09:23 +0400

    Added first test and some sinon related stuff

---
+++ src/ChpokkWeb/Content/scripts/tests/intel.tests.js
@@ -0,0 +1,29 @@
+﻿function IntelManager(editor, container) {
+    this.editor = editor;
+    this.container = container;
+}
+
+IntelManager.prototype.showData = function () {
+    this.container.show();
+};
+
+describe("On pressing the period key", function () {
+    it("Intellisense list should show up", function () {
+        // Arrange
+        var editor = $('<div/>').appendTo('body');
+        var container = $('<div/>').hide().appendTo('body');
+        var manager = new IntelManager(editor, container);
+        Server.stubContinuation({
+            success: false,
+            errors: [
+                        { message: 'Required field', field: 'FirstName' }
+                    ]
+        });
+
+        //Act
+        manager.showData();
+
+        //Assert
+        expect(container).toBeVisible();
+    });
+});
