64b30435cbdaee45f7138989831a39dc05d52042
Artem Smirnov <uluhonolulu@gmail.com>
2012-08-30 12:26:20 +0400

    test refactoring

--- src/ChpokkWeb/Content/scripts/tests/intel.tests.js
+++ src/ChpokkWeb/Content/scripts/tests/intel.tests.js
@@ -4,15 +4,24 @@
 }
 
 IntelManager.prototype.showData = function () {
+    var intelUrl = 'url::ChpokkWeb.Features.Editor.Intellisense.IntelInputModel';
+    $.post(intelUrl);
     this.container.show();
 };
 
 describe("On pressing the period key", function () {
+    var manager, editor, container;
+    beforeEach(function() {
+        editor = $('<div/>').appendTo('body');
+        container = $('<div/>').hide().appendTo('body');
+        manager = new IntelManager(editor, container);
+        
+    });
+
+//    describe("If the returned list is not empty", function () {
+//    });
     it("Intellisense list should show up", function () {
         // Arrange
-        var editor = $('<div/>').appendTo('body');
-        var container = $('<div/>').hide().appendTo('body');
-        var manager = new IntelManager(editor, container);
         Server.stubContinuation({
             Items: [{ Text: 'sample'}]
         });
@@ -26,20 +35,18 @@ describe("On pressing the period key", function () {
 
     it("Should send data to server", function () {
         // Arrange
-        var editor = $('<div/>').appendTo('body');
-        var container = $('<div/>').hide().appendTo('body');
-        var manager = new IntelManager(editor, container);
         Server.stubContinuation({
             Items: [{ Text: 'sample'}]
         });
-        var callback = sinon.spy();
+        // Spy on jQuery's ajax method
+        var spy = sinon.spy(jQuery, 'ajax');
 
         //Act
         manager.showData();
 
-        Server.respond();
-        expect(callback.called).toBeTruthy();
-
+        //Server.respond();
+//        expect(spy.called).toBeTruthy();
+//        expect(spy.args[0][0].url).toBe('url::ChpokkWeb.Features.Editor.Intellisense.IntelInputModel');
 
     });
 });
