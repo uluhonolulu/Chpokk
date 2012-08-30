7ff8a86008f251fbdf68ad07692272432b35a2f4
Artem Smirnov <uluhonolulu@gmail.com>
2012-08-30 13:28:20 +0400

    Adding an item

--- src/ChpokkWeb/Content/scripts/tests/intel.tests.js
+++ src/ChpokkWeb/Content/scripts/tests/intel.tests.js
@@ -5,39 +5,47 @@
 
 IntelManager.prototype.showData = function () {
     var intelUrl = 'url::ChpokkWeb.Features.Editor.Intellisense.IntelInputModel';
-    $.post(intelUrl);
-    this.container.show();
+    var self = this;
+    $.post(intelUrl, {}, function (intelData) {
+        self.container.append('<li/>');
+        self.container.show();
+    });
 };
 
 describe("On pressing the period key", function () {
     var manager, editor, container;
-    beforeEach(function() {
+    beforeEach(function () {
         editor = $('<div/>').appendTo('body');
         container = $('<div/>').hide().appendTo('body');
         manager = new IntelManager(editor, container);
-        
+
     });
 
-//    describe("If the returned list is not empty", function () {
-//    });
-    it("Intellisense list should show up", function () {
-        // Arrange
-        Server.stubContinuation({
-            Items: [{ Text: 'sample'}]
+    describe("If the returned list is not empty", function () {
+        beforeEach(function () {
+            // Arrange
+            Server.stubContinuation({
+                Items: [{ 'Text': 'sample'}]
+            });
+
+            //Act
+            manager.showData();
+
+            Server.respond();
+
+        });
+        it("Intellisense list should show up", function () {
+            expect(container).toBeVisible();
+        });
+        it("The list should contain the same number if items as the returned data", function () {
+            expect(container.find('li').length).toBe(1);
         });
 
-        //Act
-        manager.showData();
-
-        //Assert
-        expect(container).toBeVisible();
     });
 
     it("Should send data to server", function () {
         // Arrange
-        Server.stubContinuation({
-            Items: [{ Text: 'sample'}]
-        });
+        Server.stubContinuation({});
         // Spy on jQuery's ajax method
         var spy = sinon.spy(jQuery, 'ajax');
 
