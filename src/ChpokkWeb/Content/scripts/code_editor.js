a8a1da522827483de5c1ace940c2b3caa4e40ff2
Artem Smirnov <uluhonolulu@gmail.com>
2012-09-02 22:10:58 +0400

    Moved key handling to code_editor

--- src/ChpokkWeb/Content/scripts/code_editor.js
+++ src/ChpokkWeb/Content/scripts/code_editor.js
@@ -1,3 +1,43 @@
-﻿function CodeEditor() {
-    
+﻿$(function () {
+    $.fn.keyz.keymap.space = $.ui.keyCode.SPACE;
+});
+
+function CodeEditor(element, model) {
+    this.element = element;
+    this.intelContainer = $('#' + element.data('intelResults'));
+    var intelManager = new IntelManager($('#code'), $('#intel_results'));
+    $('#code').keyz(null, null, {
+        'space': function () {
+            colorize(this);
+            return true;
+        },
+
+        '.': function () {
+            //displayIntellisense(this);
+            intelManager.showData();
+            return true;
+            function displayIntellisense(editor) {
+                var intelData = {
+                    container: $('#intel_results'),
+                    editor: $('#code'),
+                    show: function () {
+                        //container.hide();
+                        var text = this.editor.text();
+                        //var position = this.editor.caret().start;
+                        var position = text.indexOf('""') + 2;
+                        var intelUrl = 'url::ChpokkWeb.Features.Editor.Intellisense.IntelInputModel';
+                        $.post(intelUrl, { Text: text, Position: position, NewChar: '.', RepositoryName: 'Chpokk-SampleSol', ProjectPath: 'src\\ConsoleApplication1\\ConsoleApplication1.csproj' }, function (inteldata) {
+                            if (inteldata != null && inteldata.Items != null && inteldata.Items.length > 0) {
+                                alert("it works!");
+                            }
+
+                        });
+
+                    }
+                };
+                intelData.show();
+            }
+
+        }
+    });
 }
--- src/ChpokkWeb/Content/scripts/editor.js
+++ src/ChpokkWeb/Content/scripts/editor.js
@@ -12,40 +12,7 @@ function initEditor() {
     // and CodeEditor too
     $.fn.keyz.keymap.space = $.ui.keyCode.SPACE;
     var intelManager = new IntelManager($('#code'), $('#intel_results'));
-    $('#code').keyz(null, null, {
-        'space': function (ctl, sft, alt, event) {
-            colorize(this);
-            return true;
-        },
 
-        '.': function () {
-            //displayIntellisense(this);
-            intelManager.showData();
-            return true;
-            function displayIntellisense(editor) {
-                var intelData = {
-                    container: $('#intel_results'),
-                    editor: $('#code'),
-                    show: function () {
-                        //container.hide();
-                        var text = this.editor.text();
-                        //var position = this.editor.caret().start;
-                        var position = text.indexOf('""') + 2;
-                        var intelUrl = 'url::ChpokkWeb.Features.Editor.Intellisense.IntelInputModel';
-                        $.post(intelUrl, { Text: text, Position: position, NewChar: '.', RepositoryName: 'Chpokk-SampleSol', ProjectPath: 'src\\ConsoleApplication1\\ConsoleApplication1.csproj' }, function (inteldata) {
-                            if (inteldata != null && inteldata.Items != null && inteldata.Items.length > 0) {
-                                alert("it works!");
-                            }
-
-                        });
-
-                    }
-                };
-                intelData.show();
-            }
-
-        }
-    });
 }
 
 
