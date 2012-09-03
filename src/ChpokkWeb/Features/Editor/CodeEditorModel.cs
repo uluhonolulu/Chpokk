33b4d464ab2b4a31c36b3edca52e8e546b4edae8
Artem Smirnov <uluhonolulu@gmail.com>
2012-09-03 16:26:29 +0400

    Moved it to Project.spark

--- src/ChpokkWeb/Features/Editor/CodeEditorModel.cs
+++ src/ChpokkWeb/Features/Editor/CodeEditorModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ChpokkWeb.Features.Editor {
+﻿namespace ChpokkWeb.Features.Editor {
 	public class CodeEditorModel {
 		public string Content { get; set; }
 		public string RepositoryName { get; set; }
