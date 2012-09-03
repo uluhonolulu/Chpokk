9e724b0bd807ddd124a4ac2358fec46ea2a9294b
Artem Smirnov <uluhonolulu@gmail.com>
2012-09-03 11:53:57 +0400

    Selection debugging, please ignore

--- src/ChpokkWeb/Content/scripts/intellisense.js
+++ src/ChpokkWeb/Content/scripts/intellisense.js
@@ -9,7 +9,11 @@ IntelManager.prototype.showData = function () {
     var intelUrl = 'url::ChpokkWeb.Features.Editor.Intellisense.IntelInputModel';
     var self = this;
     var text = this.editor.text();
+    var selection = window.getSelection();
     var position = text.indexOf('""') + 2;
+    var recentChars = text.substring(position - 5, position);
+    trace(recentChars);
+    debugger;
     $.post(intelUrl, { Text: text, Position: position, NewChar: '.', RepositoryName: this.model.RepositoryName, ProjectPath: this.model.ProjectPath }, function (intelData) {
         $.tmpl(self.listItemTemplate, intelData.Items).appendTo(self.container);
         self.container.show();
