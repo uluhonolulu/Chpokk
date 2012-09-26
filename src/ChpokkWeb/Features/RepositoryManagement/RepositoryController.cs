2895b879a3f14d59f4d59bff6c99e00abe6148de
Artem Smirnov <uluhonolulu@gmail.com>
2012-09-26 15:15:07 +0400

    We can see the list of repositorines on the Main page

--- src/ChpokkWeb/Features/RepositoryManagement/RepositoryController.cs
+++ src/ChpokkWeb/Features/RepositoryManagement/RepositoryController.cs
@@ -1,4 +1,7 @@
-﻿using FubuMVC.Core;
+﻿using System.Collections.Generic;
+using ChpokkWeb.Features.Exploring;
+using ChpokkWeb.Infrastructure;
+using FubuMVC.Core;
 
 namespace ChpokkWeb.Features.RepositoryManagement {
 	public class RepositoryController {
@@ -6,5 +9,22 @@ namespace ChpokkWeb.Features.RepositoryManagement {
 		public RepositoryModel Get(RepositoryInputModel input) {
 			return new RepositoryModel(){Name = input.Name};
 		}
+		[NotNull]
+		private readonly RepositoryManager _manager;
+		public RepositoryController([NotNull]RepositoryManager manager) {
+			_manager = manager;
+		}
+
+		public RepositoryListModel GetRepositoryList([NotNull]RepositoryListInputModel model) {
+			return new RepositoryListModel {RepositoryNames = _manager.GetRepositoryNames(model.PhysicalApplicationPath)};
+		}
+	}
+
+	public class RepositoryListModel {
+		public IEnumerable<string> RepositoryNames { get; set; }	
+	}
+
+	public class RepositoryListInputModel {
+		public string PhysicalApplicationPath { get; set; }	
 	}
 }
