5d97e87d3a0d310641eab0b73b469e2e8e07334b
Artem Smirnov <uluhonolulu@gmail.com>
2013-03-03 15:32:28 +0200

    Added a Push controller

---
+++ src/ChpokkWeb/Features/Remotes/Push/PushController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibGit2Sharp;
+
+namespace ChpokkWeb.Features.Remotes.Push {
+	public class PushController {
+		public void Push(string path) {
+			using (Repository repo = new Repository(path)) {
+				//Remote remote = repo.Remotes["origin"];
+				//PushResult pushResult = repo.Network.Push(remote, "HEAD", "refs/heads/destination_branch");
+				//if (pushResult.HasErrors) {
+				//    Console.WriteLine("Errors:");
+				//    foreach (PushStatusError error in pushResult.FailedPushUpdates) {
+				//        Console.WriteLine("\t{0} : {1}", error.Reference, error.Message);
+				//    }
+				//}
+			}
+		}
+	}
+}
