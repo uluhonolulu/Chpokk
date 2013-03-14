734f55283bfa762183e5d8ffae9c76f3b027c7a7
Artem Smirnov <uluhonolulu@gmail.com>
2013-03-14 19:02:13 +0200

    Created ProjectFactory.cs

---
+++ src/ChpokkWeb/Features/ProjectManagement/ProjectFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChpokkWeb.Features.ProjectManagement {
+	public class ProjectFactory {
+		public ProjectData GetProjectData(string projectFilePath) {
+			return new ProjectData();
+		}
+	}
+}
