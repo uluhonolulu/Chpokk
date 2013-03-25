d1382a40928bf4bb3c55d840a6be0677f6ecd693
Artem Smirnov <uluhonolulu@gmail.com>
2013-03-25 12:22:41 +0200

    Moving files around, created a context for references

--- src/Chpokk.Tests/ProjectLoading/ProjectFileWithBCLReferenceContext.cs
+++ src/Chpokk.Tests/ProjectLoading/ProjectFileWithBCLReferenceContext.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Chpokk.Tests.Exploring;
+using Chpokk.Tests.Exploring.UnitTests;
 
 namespace Chpokk.Tests.ProjectLoading {
-	public class ProjectFileWithBCLReferenceContext {
+	public class ProjectFileWithBCLReferenceContext : ProjectFileContext {
+		public override string ProjectFileContent {
+			get {
+				return ProjectContentWithOneBclReferenceContext.PROJECT_FILE_CONTENT;
+			}
+		}
 	}
 }
