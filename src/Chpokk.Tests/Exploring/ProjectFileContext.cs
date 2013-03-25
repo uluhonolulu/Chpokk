aa5c49c99b3d4944bb81f2b2655a5d162baf62ba
Artem Smirnov <uluhonolulu@gmail.com>
2013-03-25 12:18:42 +0200

    Refactored the project file context into an abstract class

---
+++ src/Chpokk.Tests/Exploring/ProjectFileContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FubuCore;
+
+namespace Chpokk.Tests.Exploring {
+	public abstract class ProjectFileContext : RepositoryFolderContext {
+		public readonly string SOLUTION_FOLDER = "src";
+		public readonly string PROJECT_NAME = "ProjectName";
+		public readonly string PROJECT_PATH = @"ProjectName\ProjectName.csproj";
+		public readonly string FILE_NAME = "Class1.cs";
+		public string SolutionFolder { get; set; }
+		public abstract string ProjectFileContent { get; }
+
+		public override void Create() {
+			base.Create();
+			SolutionFolder = FileSystem.Combine(RepositoryRoot, SOLUTION_FOLDER);
+			var projectPath = FileSystem.Combine(SolutionFolder, PROJECT_PATH);
+			Console.WriteLine("Writing to " + projectPath);
+			Container.Get<IFileSystem>().WriteStringToFile(projectPath, ProjectFileContent);
+		}
+	}
+}
--- src/Chpokk.Tests/Exploring/ProjectWithSingleRootFileContext.cs
+++ src/Chpokk.Tests/Exploring/ProjectWithSingleRootFileContext.cs
@@ -2,28 +2,16 @@
 using FubuCore;
 
 namespace Chpokk.Tests.Exploring {
-	public class ProjectWithSingleRootFileContext : GitRepositoryContext {
-		public readonly string SOLUTION_FOLDER = "src";
+	public class ProjectWithSingleRootFileContext : ProjectFileContext {
 		//public string PROJECT_ROOT = "root";
-		public readonly string PROJECT_NAME = "ProjectName";
-		public readonly string PROJECT_PATH = @"ProjectName\ProjectName.csproj";
-		public readonly string FILE_NAME = "Class1.cs";
-		public string PROJECT_FILE_CONTENT =
-			@"<?xml version=""1.0"" encoding=""utf-8""?>
+
+		public override string ProjectFileContent {
+			get { return @"<?xml version=""1.0"" encoding=""utf-8""?>
 				<Project ToolsVersion=""4.0"" DefaultTargets=""Build"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
 				  <ItemGroup>
 					<Compile Include=""Class1.cs"" />
 				  </ItemGroup>
-				</Project>";
-
-		public string SolutionFolder { get; set; }
-
-		public override void Create() {
-			base.Create();
-			SolutionFolder = FileSystem.Combine(RepositoryRoot, SOLUTION_FOLDER);
-			var projectPath = FileSystem.Combine(SolutionFolder, PROJECT_PATH);
-			Console.WriteLine("Writing to " + projectPath);
-			Container.Get<IFileSystem>().WriteStringToFile(projectPath, PROJECT_FILE_CONTENT);
+				</Project>"; }
 		}
 	}
 }
