22357a4392b22f4f9a1988b9eb0a8263bf807cc2
Artem Smirnov <uluhonolulu@gmail.com>
2013-04-17 09:35:25 +0300

    Properly handling Janrain -- from AJAX to page request

--- src/ChpokkWeb/Features/Authorization/LoginEndpoint.cs
+++ src/ChpokkWeb/Features/Authorization/LoginEndpoint.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
+using ChpokkWeb.Features.MainScreen;
 using FubuMVC.Core.Ajax;
+using FubuMVC.Core.Continuations;
 using FubuMVC.Core.Security;
 using Newtonsoft.Json;
 
@@ -14,7 +16,7 @@ namespace ChpokkWeb.Features.Authorization {
 			_authenticationContext = authenticationContext;
 		}
 
-		public AjaxContinuation Login(LoginInputModel model) {
+		public FubuContinuation Login(LoginInputModel model) {
 			var url = string.Format("https://rpxnow.com/api/v2/auth_info?apiKey={0}&token={1}", model.ApiKey, model.token);
 			var rawResponse =
 				new WebClient().DownloadString(url);
@@ -22,9 +24,10 @@ namespace ChpokkWeb.Features.Authorization {
 			if (response.stat.ToString() == "ok") {
 				var username = response.profile.preferredUsername.ToString();
 				_authenticationContext.ThisUserHasBeenAuthenticated(username, true);
-				return AjaxContinuation.Successful();
+				return FubuContinuation.RedirectTo<MainDummyModel>();
 			}
-			return AjaxContinuation.ForMessage(response.stat.ToString());
+			return FubuContinuation.EndWithStatusCode(HttpStatusCode.Unauthorized);
+			//return AjaxContinuation.ForMessage(response.stat.ToString());
 		}
 	}
 
---
+++ src/ChpokkWeb/Features/MainScreen/MainDummyModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChpokkWeb.Shared;
+
+namespace ChpokkWeb.Features.MainScreen {
+	public class MainDummyModel: IDontNeedActionsModel {
+	}
+}
