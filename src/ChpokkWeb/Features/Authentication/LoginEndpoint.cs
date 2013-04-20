ba8ba51ab98c6e3809fc53f2bc8bfd0766ad4f0e
Artem Smirnov <uluhonolulu@gmail.com>
2013-04-20 22:12:13 +0300

    tracking auth stuff

--- src/ChpokkWeb/Features/Authentication/LoginEndpoint.cs
+++ src/ChpokkWeb/Features/Authentication/LoginEndpoint.cs
@@ -7,11 +7,16 @@ using Newtonsoft.Json;
 namespace ChpokkWeb.Features.Authentication {
 	public class LoginEndpoint {
 		private readonly IAuthenticationContext _authenticationContext;
-		public LoginEndpoint(IAuthenticationContext authenticationContext) {
+		private ISecurityContext _securityContext;
+		public LoginEndpoint(IAuthenticationContext authenticationContext, ISecurityContext securityContext) {
 			_authenticationContext = authenticationContext;
+			_securityContext = securityContext;
 		}
 
 		public FubuContinuation Login(LoginInputModel model) {
+			_authenticationContext.ThisUserHasBeenAuthenticated("ulu", true);
+			return FubuContinuation.RedirectTo<ShowMeModel>();
+
 			var url = string.Format("https://rpxnow.com/api/v2/auth_info?apiKey={0}&token={1}", model.ApiKey, model.token);
 			var rawResponse =
 				new WebClient().DownloadString(url);
@@ -24,6 +29,12 @@ namespace ChpokkWeb.Features.Authentication {
 			return FubuContinuation.EndWithStatusCode(HttpStatusCode.Unauthorized);
 			//return AjaxContinuation.ForMessage(response.stat.ToString());
 		}
+
+		public string ShowMe(ShowMeModel model) {
+			return _securityContext.IsAuthenticated().ToString();
+		}
+
+		public class ShowMeModel {}
 	}
 
 	public class LoginInputModel {
