a1bba6a7fc54aa83709a80c213cba36a6bff81ce
Artem Smirnov <uluhonolulu@gmail.com>
2013-04-25 22:13:57 +0300

    Using username for commits

--- src/ChpokkWeb/Features/Remotes/SaveCommit/SaveCommitController.cs
+++ src/ChpokkWeb/Features/Remotes/SaveCommit/SaveCommitController.cs
@@ -1,14 +1,18 @@
-﻿using ChpokkWeb.Features.Exploring;
+﻿using System;
+using ChpokkWeb.Features.Exploring;
 using FubuCore;
+using FubuMVC.Core.Security;
 using LibGit2Sharp;
 
 namespace ChpokkWeb.Features.Remotes.SaveCommit {
 	public class SaveCommitController {
 		private readonly FileSystem _fileSystem;
-		private RepositoryManager _manager;
-		public SaveCommitController(FileSystem fileSystem, RepositoryManager manager) {
+		private readonly RepositoryManager _manager;
+		private ISecurityContext _securityContext;
+		public SaveCommitController(FileSystem fileSystem, RepositoryManager manager, ISecurityContext securityContext) {
 			_fileSystem = fileSystem;
 			_manager = manager;
+			_securityContext = securityContext;
 		}
 
 		public void Save(SaveCommitModel saveCommitModel) {
@@ -17,9 +21,11 @@ namespace ChpokkWeb.Features.Remotes.SaveCommit {
 			if (saveCommitModel.DoCommit) {
 				var repositoryInfo = _manager.GetRepositoryInfo(saveCommitModel.RepositoryName);
 				var repositoryPath = saveCommitModel.PhysicalApplicationPath.AppendPath(repositoryInfo.Path);
+				var userName = _securityContext.CurrentIdentity.Name;
+				var author = new Signature(userName, userName, DateTimeOffset.Now);
 				using (var repo = new Repository(repositoryPath)) {
 					repo.Index.Stage(filePath);
-					repo.Commit(saveCommitModel.CommitMessage);
+					repo.Commit(saveCommitModel.CommitMessage, author);
 				}
 			}
 		}
