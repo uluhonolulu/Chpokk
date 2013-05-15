fd552fd88cbb0eb9346f9b36ba5960812017cc95
Artem Smirnov <uluhonolulu@gmail.com>
2013-05-15 21:51:07 +0300

    Home page is fine Endpoints are fine

---
+++ src/Chpokk.Tests/Spikes/RegisterEndpoints.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CThru.BuiltInAspects;
+using FubuMVC.Core.Registration;
+using Gallio.Framework;
+using Ivonna.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using Ivonna.Framework.Generic;
+
+namespace Chpokk.Tests.Spikes {
+	[TestFixture, RunOnWeb]
+	public class RegisterEndpoints {
+		[Test]
+		public void TraceAllActionSourceCalls	() {
+			var session = new TestSession();
+			session.AddAspect(new TraceAspect(info => info.TargetInstance is ActionSource));
+			session.Get("Main");
+		}
+	}
+}
--- src/ChpokkWeb/Features/Demo/DemoModel.cs
+++ src/ChpokkWeb/Features/Demo/DemoModel.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using ChpokkWeb.Infrastructure;
+using FubuMVC.Core;
 
 namespace ChpokkWeb.Features.Demo {
+	[UrlPattern("")]
 	public class DemoModel: IDontNeedActionsModel {
 	}
 }
