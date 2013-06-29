5d402152ac76baacac530359c83afe4ba7a90ebc
Artem Smirnov <uluhonolulu@gmail.com>
2013-06-29 23:39:05 +0300

    Keep alive

---
+++ src/ChpokkWeb/Features/Editor/PingEndpoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FubuMVC.Core.Ajax;
+
+namespace ChpokkWeb.Features.Editor {
+	public class PingEndpoint {
+		public AjaxContinuation KeepAlive() {
+			return AjaxContinuation.Successful();
+		}
+	}
+}
