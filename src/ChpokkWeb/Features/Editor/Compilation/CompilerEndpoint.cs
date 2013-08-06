dc9e5bdb41560e3cea1eda06d98e9298c0c0fc39
Artem Smirnov <uluhonolulu@gmail.com>
2013-08-06 11:06:26 +0300

    build spike

---
+++ src/ChpokkWeb/Features/Editor/Compilation/CompilerEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChpokkWeb.Features.Exploring;
+using FubuCore;
+using FubuMVC.Core.Ajax;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+
+namespace ChpokkWeb.Features.Editor.Compilation {
+	public class CompilerEndpoint {
+		public AjaxContinuation DoIt(CompileInputModel model) {
+			var projectCollection = ProjectCollection.GlobalProjectCollection;
+			var logger = new ChpokkLogger();
+			projectCollection.RegisterLogger(logger);
+			var projectFilePath = FileSystem.Combine(model.PhysicalApplicationPath,
+										  @"UserFiles\uluhonolulu\Chpokk-SampleSol\src\ConsoleApplication1\ConsoleApplication1.csproj");
+			var project = projectCollection.LoadProject(projectFilePath);
+			bool result = project.Build();
+			var ajaxContinuation = AjaxContinuation.Successful();
+			ajaxContinuation.Message = "Success: " + result.ToString();
+			ajaxContinuation.Errors.AddRange(from message in logger.Messages select new AjaxError() {message = message});
+			return ajaxContinuation;
+		}
+	}
+
+	public class CompileInputModel: BaseRepositoryInputModel {}
+
+	public class ChpokkLogger: ILogger {
+		public ChpokkLogger() {
+			Messages = new List<string>();
+		}
+
+		public List<string> Messages { get; set; }
+		public void Initialize(IEventSource eventSource) {
+			eventSource.AnyEventRaised += (sender, args) => Messages.Add(args.Message);
+		}
+		public void Shutdown() {}
+		public LoggerVerbosity Verbosity { get; set; }
+		public string Parameters { get; set; }
+	}
+}
