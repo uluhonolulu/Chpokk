53799624639d4206ad2c06aa417fc4b5f36e9455
Artem Smirnov <uluhonolulu@gmail.com>
2013-08-21 13:43:29 +0300

    Check for null in GetLanguage

--- src/ChpokkWeb/Features/LanguageSupport/LanguageDetector.cs
+++ src/ChpokkWeb/Features/LanguageSupport/LanguageDetector.cs
@@ -10,11 +10,13 @@ namespace ChpokkWeb.Features.LanguageSupport {
 	public class LanguageDetector {
 		public SupportedLanguage GetLanguage(string fileName) {
 			var ext = Path.GetExtension(fileName);
-			if (ext.Equals(".cs", StringComparison.InvariantCultureIgnoreCase) || ext.Equals(".csproj", StringComparison.InvariantCultureIgnoreCase)) {
-				return SupportedLanguage.CSharp;
-			}
-			if (ext.Equals(".vb", StringComparison.InvariantCultureIgnoreCase) || ext.Equals(".vbproj", StringComparison.InvariantCultureIgnoreCase)) {
-				return SupportedLanguage.VBNet;
+			if (ext != null) {
+				if (ext.Equals(".cs", StringComparison.InvariantCultureIgnoreCase) || ext.Equals(".csproj", StringComparison.InvariantCultureIgnoreCase)) {
+					return SupportedLanguage.CSharp;
+				}
+				if (ext.Equals(".vb", StringComparison.InvariantCultureIgnoreCase) || ext.Equals(".vbproj", StringComparison.InvariantCultureIgnoreCase)) {
+					return SupportedLanguage.VBNet;
+				} 
 			}
 			throw new Exception("Language not detected for " + fileName);
 		}
