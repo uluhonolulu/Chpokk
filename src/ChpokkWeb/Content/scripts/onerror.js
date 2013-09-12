d169426cc78d7109dceae9953bd83ded5e59c4f2
Artem Smirnov <uluhonolulu@gmail.com>
2013-09-12 13:46:53 +0300

    error message for the localStorage problem

--- src/ChpokkWeb/Content/scripts/onerror.js
+++ src/ChpokkWeb/Content/scripts/onerror.js
@@ -5,4 +5,16 @@ window.onerror = function (exception, url, line) {
 	var targetUrl = 'url::ChpokkWeb.Features.CustomerDevelopment.ErrorModel';
 	$.post(targetUrl, data);
 };
-//throw new Error("Oh oh, an error has occured");
+
+//problem with local storage
+var localStorageOk = true;
+try {
+	var storage = window.localStorage;
+} catch(e) {
+	localStorageOk = false;
+}
+if (!localStorageOk) {
+	alert("Your browser's security settings are not compatible with some of the Chpokk's features. \r\nPlease run your browser as administrator, or use a different browser.");
+}
+
+
