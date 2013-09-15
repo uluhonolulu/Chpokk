cfb54d2bfe13332a6dc9027f89c8ab398b7d9127
Artem Smirnov <uluhonolulu@gmail.com>
2013-09-15 22:32:42 +0300

    Compilation code for testing

---
+++ src/Chpokk.Tests/Run/AssemblyLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+
+namespace Chpokk.Tests.Run {
+	public class AssemblyLoader: MarshalByRefObject {
+		public void Run(string exePath) {
+			var assembly = Assembly.LoadFrom(exePath);
+			assembly.EntryPoint.Invoke(null, new object[]{null});
+		}
+	}
+}
--- src/Chpokk.Tests/Run/RunningValidCode.cs
+++ src/Chpokk.Tests/Run/RunningValidCode.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Reflection;
 using System.Text;
 using Gallio.Framework;
 using MbUnit.Framework;
 using MbUnit.Framework.ContractVerifiers;
+using FubuCore;
 
 namespace Chpokk.Tests.Run {
 	[TestFixture]
 	public class RunningValidCode {
+		[Test]
+		public void CanCompile() {
+			var code = "class program {static void Main(){}}";
+			var path = @"D:\Projects\Chpokk\src\ChpokkWeb\UserFiles\program.cs";
+			var exePath = Compile(path, code);
+			Assert.IsTrue(File.Exists(exePath));
+		}
+
+		public static string Compile(string path, string code) {
+			File.WriteAllText(path, code);
+			var fileName = Path.GetFileName(path);
+			var startInfo = new ProcessStartInfo(@"C:\Windows\Microsoft.NET\Framework\v4.0.30319\Csc.exe",
+			                                     "/utf8output " + fileName)
+				{
+					RedirectStandardOutput = true,
+					RedirectStandardError = true,
+					UseShellExecute = false,
+					WorkingDirectory = path.ParentDirectory(),
+					StandardOutputEncoding = Encoding.UTF8,
+					CreateNoWindow = true
+				};
+			using (var process = Process.Start(startInfo)) {
+				process.WaitForExit();
+				Console.WriteLine(process.StandardOutput.ReadToEnd());
+				Console.WriteLine(process.StandardError.ReadToEnd());
+			}
+			var exeFileName = Path.GetFileNameWithoutExtension(fileName) + ".exe";
+			var exePath = Path.Combine(path.ParentDirectory(), exeFileName);
+			return exePath;
+		}
+
 		[Test]
 		public void Test() {
 			var path =
@@ -21,12 +53,5 @@ namespace Chpokk.Tests.Run {
 			loader.Run(path);
 			AppDomain.Unload(appDomain);
 		}
-	}
-
-	public class AssemblyLoader: MarshalByRefObject {
-	    public void Run(string exePath) {
-		    var assembly = Assembly.LoadFrom(exePath);
-			assembly.EntryPoint.Invoke(null, new object[]{null});
-	    }
 	}
 }
