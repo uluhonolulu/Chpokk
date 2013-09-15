14c6035216124553864ef368c30515dc7342d64a
Artem Smirnov <uluhonolulu@gmail.com>
2013-09-15 19:27:30 +0300

    First take on Run

---
+++ src/Chpokk.Tests/Run/RunningValidCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace Chpokk.Tests.Run {
+	[TestFixture]
+	public class RunningValidCode {
+		[Test]
+		public void Test() {
+			var path =
+				@"D:\Projects\Chpokk\src\ChpokkWeb\UserFiles\__anonymous__\Chpokk-SampleSol\src\ConsoleApplication1\bin\Debug\ConsoleApplication1.exe";
+			var assembly = Assembly.LoadFile(path);
+			assembly.EntryPoint.Invoke(null, new object[]{null});
+			//
+		}
+	}
+}
