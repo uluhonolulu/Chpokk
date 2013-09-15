d02f44d2a963003601b594c6337edf5cd64dc6ed
Artem Smirnov <uluhonolulu@gmail.com>
2013-09-15 20:53:31 +0300

    Loading the Loader to a different AppDomain

--- src/Chpokk.Tests/Run/RunningValidCode.cs
+++ src/Chpokk.Tests/Run/RunningValidCode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Text;
 using Gallio.Framework;
@@ -13,9 +14,18 @@ namespace Chpokk.Tests.Run {
 		public void Test() {
 			var path =
 				@"D:\Projects\Chpokk\src\ChpokkWeb\UserFiles\__anonymous__\Chpokk-SampleSol\src\ConsoleApplication1\bin\Debug\ConsoleApplication1.exe";
-			var assembly = Assembly.LoadFile(path);
-			assembly.EntryPoint.Invoke(null, new object[]{null});
+			var appDomain = AppDomain.CreateDomain("runner", null, @"D:\Projects\Chpokk\src\ChpokkWeb\UserFiles\__anonymous__\Chpokk-SampleSol\src\ConsoleApplication1\bin\Debug\", "", false);
+			var loader = (AssemblyLoader) appDomain.CreateInstanceFromAndUnwrap(typeof(AssemblyLoader).Assembly.CodeBase,
+			                                                                               typeof (AssemblyLoader).FullName, null);
+			Assert.DoesNotExist(AppDomain.CurrentDomain.GetAssemblies(), assembly => assembly.FullName.Contains("ConsoleApplication1"));
 			//
 		}
+	}
+
+	public class AssemblyLoader: MarshalByRefObject {
+	    public void Run(string exePath) {
+		    var assembly = Assembly.LoadFrom(exePath);
+			assembly.EntryPoint.Invoke(null, new object[]{null});
+	    }
 	}
 }
