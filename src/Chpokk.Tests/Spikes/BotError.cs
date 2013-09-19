6cad48f43f2d02cf1665c7eb975ccf49613349df
Artem Smirnov <uluhonolulu@gmail.com>
2013-09-19 16:15:47 +0300

    bot error test

---
+++ src/Chpokk.Tests/Spikes/BotError.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Gallio.Framework;
+using Ivonna.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using WebRequest = Ivonna.Framework.Generic.WebRequest;
+
+namespace Chpokk.Tests.Spikes {
+	[TestFixture, RunOnWeb, Ignore]
+	public class BotError {
+		[Test]
+		public void Test() {
+			var session = new TestSession();
+			var request = new WebRequest("/");
+			request.Headers.Add(HttpRequestHeader.Accept, "*/*");
+			request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+			request.Headers.Add(HttpRequestHeader.Connection, "close");
+			request.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
+			request.Headers.Add(HttpRequestHeader.From, "googlebot(at)googlebot.com");
+			request.Headers.Add(HttpRequestHeader.Host, "chpokk.apphb.com");
+			var response = session.ProcessBaseRequest(request);
+			Console.WriteLine(response.BodyAsString);
+		}
+	}
+}
