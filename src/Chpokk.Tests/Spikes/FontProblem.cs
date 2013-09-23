900b832772326a6c14f574599dbd1c08b4f3c779
Artem Smirnov <uluhonolulu@gmail.com>
2013-09-23 20:02:46 +0300

    fixed missing git binaries

---
+++ src/Chpokk.Tests/Spikes/FontProblem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CThru.BuiltInAspects;
+using Gallio.Framework;
+using Ivonna.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using Ivonna.Framework.Generic;
+using Shouldly;
+
+namespace Chpokk.Tests.Spikes {
+	[TestFixture, RunOnWeb]
+	public class FontProblem {
+		[Test]
+		public void CanDownloadAFont() {
+			var session = new TestSession();
+			session.AddAspect(new TraceAspect(info => info.TargetInstance is Exception, 10));
+			session.AddAspect(new TraceAspect(info => info.TypeName.Contains("Asset")));
+			var response = session.Get("_content/styles/fonts/glyphicons-halflings-regular.woff");
+			response.Status.ShouldBe(200);
+		}
+	}
+}
