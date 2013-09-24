82dc70ade9c07de235c5a8b1568c84b43de9c70c
Artem Smirnov <uluhonolulu@gmail.com>
2013-09-24 11:25:30 +0300

    Added smoke tests, and testing that the git dll is there

---
+++ src/SmokeTests/LibGitCanInit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace SmokeTests {
+	[TestFixture]
+	public class LibGitCanInit {
+		[Test]
+		public void WithoutExceptions() {
+			var path = Path.GetFullPath(@"..\..\..\ChpokkWeb\bin\NativeBinaries\amd64");
+			Console.WriteLine(path);
+			Assert.IsNotEmpty(Directory.GetFiles(path, "git2*.dll"));
+			
+		}
+	}
+}
