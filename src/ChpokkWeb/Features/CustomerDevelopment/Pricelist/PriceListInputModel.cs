a7f54be49cd76eac765455da206f547455b183f8
Artem Smirnov <uluhonolulu@gmail.com>
2013-09-26 10:05:27 +0300

    Price list WIP

---
+++ src/ChpokkWeb/Features/CustomerDevelopment/Pricelist/PriceListInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FubuMVC.Core;
+
+namespace ChpokkWeb.Features.CustomerDevelopment.Pricelist {
+	[UrlPattern("pricing")]
+	public class PriceListInputModel {
+	}
+}
