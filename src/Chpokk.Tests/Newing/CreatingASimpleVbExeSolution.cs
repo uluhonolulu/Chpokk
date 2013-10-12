93097e3964ddcfd033ac75ba4497119f4ec9de3a
Artem Smirnov <uluhonolulu@gmail.com>
2013-10-12 18:02:38 +0300

    dashboard WIP

---
+++ src/Chpokk.Tests/Newing/CreatingASimpleVbExeSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Arractas;
+using ChpokkWeb.Features.ProjectManagement.AddSimpleProject;
+using ChpokkWeb.Features.RepositoryManagement;
+using Gallio.Framework;
+using ICSharpCode.NRefactory;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using Microsoft.Build.Evaluation;
+using Shouldly;
+
+namespace Chpokk.Tests.Newing {
+	[TestFixture]
+	public class CreatingASimpleVbExeSolution : BaseCommandTest<RepositoryCleanupContext> {
+		public const string NAME = "Carramba";
+		[Test]
+		public void ProducesABuildableProject() {
+			new Project(ProjectPath).Build(new CreatingASimpleSolution.ConsoleBuildLogger()).ShouldBe(true);
+			var binPath = RepositoryManager.GetAbsolutePathFor(NAME, Context.AppRoot, Path.Combine(NAME, "bin"));
+			Directory.Exists(binPath).ShouldBe(true);
+		}
+
+		public override void Act() {
+			var endpoint = Context.Container.Get<AddSimpleProjectEndpoint>();
+			endpoint.DoIt(new AddSimpleProjectInputModel { PhysicalApplicationPath = Context.AppRoot, RepositoryName = NAME, OutputType = "Exe", Language = SupportedLanguage.VBNet });
+		}
+
+		private string ProjectPath {
+			get { return RepositoryManager.GetAbsolutePathFor(NAME, Context.AppRoot, Path.Combine(NAME, NAME + ".vbproj")); }
+		}
+		RepositoryManager RepositoryManager { get { return Context.Container.Get<RepositoryManager>(); } }
+	}
+}
