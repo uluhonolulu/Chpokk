cb30c4405b3e32dee18e929887e917d5af5a0605
Artem Smirnov <uluhonolulu@gmail.com>
2013-10-15 19:06:39 +0300

    Smoke test for writing to a temp file

---
+++ src/SmokeTests/CanWriteToTempFolder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace SmokeTests {
+	[TestFixture]
+	public class CanWriteToTempFolder {
+		[Test]
+		public void WithoutThrowingAnException() {
+			var path = Path.GetTempFileName();
+			Console.WriteLine(path);
+			File.WriteAllText(path, string.Empty);
+			Console.WriteLine("Wrote a file");
+			File.Delete(path);
+		}
+	}
+}
