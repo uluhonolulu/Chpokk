8052ced1bd07eb20765e083777c6f7b8beeadec7
Artem Smirnov <uluhonolulu@gmail.com>
2013-10-15 22:38:42 +0300

    Smoke cloning to temp

--- src/SmokeTests/CanWriteToTempFolder.cs
+++ src/SmokeTests/CanWriteToTempFolder.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Gallio.Framework;
+using LibGit2Sharp;
 using MbUnit.Framework;
 using MbUnit.Framework.ContractVerifiers;
+using FubuCore;
 
 namespace SmokeTests {
 	[TestFixture]
 	public class CanWriteToTempFolder {
 		[Test]
-		public void WithoutThrowingAnException() {
+		public void EmptyFile() {
 			var path = Path.GetTempFileName();
 			Console.WriteLine(path);
 			File.WriteAllText(path, string.Empty);
 			Console.WriteLine("Wrote a file");
 			File.Delete(path);
+		}
+
+		[Test]
+		public void CanCloneIntoIt() {
+			var path = Path.Combine(Path.GetTempFileName().ParentDirectory(), "gr1dorg");
+			try {
+				Repository.Clone(sourceUrl: "https://git.assembla.com/gr1dorg.git", workdirPath: path,
+			                 credentials: new Credentials() {Username = "rootgr1dorg", Password = "5.25InchDisk"});
+				Directory.Move(path, Path.GetFullPath(@"_PublishedWebsites\ChpokkWeb\UserFiles\uluhonolulu_Twitter"));
+			}
+			finally {
+				if(Directory.Exists(path))
+					Directory.Delete(path, true);
+			}
+			
+				//RepoUrl=http%3A%2F%2Fgit.assembla.com%2Fgr1dorg.git&Username=rootgr1dorg&Password=5.25InchDisk
 		}
 	}
 }
