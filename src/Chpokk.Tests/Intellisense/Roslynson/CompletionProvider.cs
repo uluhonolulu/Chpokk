ba2c6ca632db29c14284012cbe19ddfd1559cf8c
Artem Smirnov <uluhonolulu@gmail.com>
2013-10-23 19:57:01 +0300

    All Three Roslyn tests are good

--- src/Chpokk.Tests/Intellisense/Roslynson/CompletionProvider.cs
+++ src/Chpokk.Tests/Intellisense/Roslynson/CompletionProvider.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Roslyn.Compilers;
 using Roslyn.Compilers.CSharp;
 using Roslyn.Compilers.Common;
 
 namespace Chpokk.Tests.Intellisense.Roslynson {
 	public class CompletionProvider {
-		public IEnumerable<ISymbol> GetSymbols(string source, int position) {
-			var tree = SyntaxTree.ParseCompilationUnit(source);
-			ICompilation compilation = Roslyn.Compilers.CSharp.Compilation.Create("Compilation", syntaxTrees: new[] { tree });
+		public IEnumerable<ISymbol> GetSymbols(string source, int position, LanguageNames language) {
+			var tree = SyntaxTree.ParseText(source);
+			switch (language) {
+					
+			}
+			CommonCompilation compilation = Roslyn.Compilers.CSharp.Compilation.Create("MyCompilation", syntaxTrees: new[] { tree });
 			var semanticModel = compilation.GetSemanticModel(tree);
 			var declaredSymbol = GetContainingClass(position, tree, semanticModel);
-			var symbols = semanticModel.LookupSymbols(position);
+			var symbols = semanticModel.LookupSymbols(position, declaredSymbol);
 
 			Console.WriteLine();
 			Console.WriteLine("symbols:");
@@ -22,18 +26,28 @@ namespace Chpokk.Tests.Intellisense.Roslynson {
 				//}
 				//Console.WriteLine();
 			}
-			return symbols;
+			return symbols.AsEnumerable();
 		}
 
 		private INamedTypeSymbol GetContainingClass(int position, CommonSyntaxTree tree, ISemanticModel semanticModel) {
-			var syntaxToken = tree.Root.FindToken(position);
+			var syntaxToken = tree.GetRoot().FindToken(position);
 			var nodeHierarchy = syntaxToken.Parent.AncestorsAndSelf();
 			foreach (var syntaxNode in nodeHierarchy) {
-				var symbol = semanticModel.GetDeclaredSymbol(syntaxNode);
-				//Console.WriteLine(syntaxNode.Kind.ToString());
+				var thisNode = syntaxNode;
+				Console.WriteLine(thisNode.GetText() + ": " + thisNode.GetType());
+				if (syntaxNode is MemberAccessExpressionSyntax) {
+					thisNode = (syntaxNode as MemberAccessExpressionSyntax).Expression;
+					Console.WriteLine("replaced with: " + thisNode.GetText() + ": " + thisNode.GetType());
+				}
+				var typeInfo = semanticModel.GetTypeInfo(thisNode);
+				Console.WriteLine("Type: " + typeInfo.Type);
+				if (typeInfo.Type != null) {
+					return (INamedTypeSymbol) typeInfo.Type;
+				}
+				var symbol = semanticModel.GetDeclaredSymbol(thisNode);
 				if (symbol != null) {
-					//Console.WriteLine("Symbol: " + symbol);
-					//Console.WriteLine("Type: " + symbol.ContainingType);
+					Console.WriteLine("Symbol: " + symbol);
+					Console.WriteLine("Type: " + symbol.ContainingType);
 					return symbol.ContainingType;
 				}
 			}
--- src/Chpokk.Tests/Intellisense/Roslynson/ThisDotMember.cs
+++ src/Chpokk.Tests/Intellisense/Roslynson/ThisDotMember.cs
@@ -19,46 +19,6 @@ namespace Chpokk.Tests.Intellisense.Roslynson {
 			symbols.Any(symbol => symbol.Name == "ClassA").ShouldBe(false);
 		}
 
-		private static IEnumerable<Roslyn.Compilers.CSharp.Symbol> GetSymbols(string source, int position) {
-			var tree = SyntaxTree.ParseCompilationUnit(source);
-			var compilation = Roslyn.Compilers.CSharp.Compilation.Create("Compilation", syntaxTrees: new[] { tree });
-			var semanticModel = compilation.GetSemanticModel(tree);
 
-			var declaredSymbol = GetContainingClass(position, tree, semanticModel);
-			//var containingSymbol = (NamespaceOrTypeSymbol)declaredSymbol.ContainingSymbol;
-			//var identifier = tree.Root.FindToken(position-1).Parent;
-			//var semanticInfo = semanticModel.GetSemanticInfo(identifier as ExpressionSyntax);
-			//var type = semanticInfo.Type;
-			var symbols = semanticModel.LookupSymbols(position, container: declaredSymbol, options: LookupOptions.IncludeExtensionMethods);
-
-			Console.WriteLine();
-			Console.WriteLine("symbols:");
-			foreach (var symbol in symbols) Console.WriteLine(symbol);
-			return symbols;
-		}
-
-		private static NamedTypeSymbol GetContainingClass(int position, SyntaxTree tree, SemanticModel semanticModel) {
-			var syntaxToken = tree.Root.FindToken(position);
-			Console.WriteLine("Token at position: " + syntaxToken);
-			var parent = syntaxToken.Parent;
-			var identifier = parent.ChildNodes().FirstOrDefault() as ExpressionSyntax;
-			Console.WriteLine("Identifier: " + identifier + "(" + identifier.GetType() + ")");
-			Console.WriteLine(semanticModel.GetDeclaredSymbol(identifier));
-			while (parent != null) { //see Symbol.cs for a better syntax
-				Console.WriteLine("Parent node: " + parent + "(" + parent.GetType() + ")");
-				var symbol = semanticModel.GetDeclaredSymbol(parent);
-				Console.WriteLine("Symbol: " +
-				                  (symbol != null ? symbol.ToString() + " - " + symbol.Kind + ", " + symbol.ContainingType : "null"));
-				if (parent is ExpressionSyntax)
-					Console.WriteLine("Type info: " + semanticModel.GetSemanticInfo((ExpressionSyntax) parent).Type);
-
-				Console.WriteLine();
-				if (symbol != null && symbol.ContainingType != null) {
-					return symbol.ContainingType;
-				}
-				parent = parent.Parent;
-			}
-			return null;
-		}
 	}
 }
