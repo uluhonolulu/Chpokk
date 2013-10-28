56fe139122d848e2d5854c37d3d30485e170a38f
Artem Smirnov <uluhonolulu@gmail.com>
2013-10-28 14:01:48 +0200

    added evergage

---
+++ src/ChpokkWeb/Content/scripts/evergage.js
@@ -0,0 +1,12 @@
+﻿var _aaq = _aaq || [];
+var evergageAccount = 'uluhonolulu';
+var dataset = 'engage';
+_aaq.push(['setEvergageAccount', evergageAccount], ['setDataset', dataset], ['setUseSiteConfig', true]);
+
+(function(){
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript'; g.defer = true; g.async = true;
+    g.src = document.location.protocol + '//cdn.evergage.com/beacon/'
+            + evergageAccount + '/' + dataset + '/scripts/evergage.min.js';
+    s.parentNode.insertBefore(g, s);
+})();
