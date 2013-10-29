3f4908db5bb8496f9e7ed0485d9192fa31f8b74f
Artem Smirnov <uluhonolulu@gmail.com>
2013-10-29 19:54:58 +0200

    Extracted BclAssembliesProvider

---
+++ src/Chpokk.Tests/References/BclAssembliesProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+
+namespace Chpokk.Tests.References {
+	public class BclAssembliesProvider {
+		public IEnumerable<string> GetBclAssemblies() {
+			var rootElement = ProjectRootElement.Create();
+			var targetImport = @"$(MSBuildToolsPath)\Microsoft.CSharp.targets";
+			rootElement.AddImport(targetImport);
+			var project = new Project(rootElement);
+			var property = project.AllEvaluatedProperties.First(projectProperty => projectProperty.Name == "FrameworkPathOverride");
+			Console.WriteLine(property.EvaluatedValue);
+			var assemblyFolder = property.EvaluatedValue;
+			var assemblyPaths = Directory.EnumerateFiles(assemblyFolder, "*.dll");
+			var assemblies = from path in assemblyPaths select Path.GetFileNameWithoutExtension(path);
+			assemblies = assemblies.Except(new[] {"mscorlib", "sysglobl"}).OrderBy(s => s);
+			return assemblies;
+		}
+	}
+}
--- src/Chpokk.Tests/References/StandardLibraryList.cs
+++ src/Chpokk.Tests/References/StandardLibraryList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using FubuCore;
@@ -6,25 +7,16 @@ using ICSharpCode.NRefactory;
 using MbUnit.Framework;
 using Microsoft.Build.Construction;
 using Microsoft.Build.Evaluation;
+using Shouldly;
 
 namespace Chpokk.Tests.References {
 	public class StandardLibraryList {
+
 		[Test]
 		public void CanRetrieveDisplaysBclAssemblies() {
-			var language = SupportedLanguage.CSharp;
-			var rootElement = ProjectRootElement.Create();
-			var targetImport = @"$(MSBuildToolsPath)\Microsoft.{0}.targets".ToFormat(language == SupportedLanguage.CSharp ? "CSharp" : "VisualBasic");
-			rootElement.AddImport(targetImport);
-			var project = new Project(rootElement);
-			var property = project.AllEvaluatedProperties.First(projectProperty => projectProperty.Name == "FrameworkPathOverride");
-			Console.WriteLine(property.EvaluatedValue);
-			var assemblyFolder = property.EvaluatedValue;
-			var assemblyPaths = Directory.EnumerateFiles(assemblyFolder, "*.dll");
-			var assemblies = from path in assemblyPaths select Path.GetFileNameWithoutExtension(path);
-			assemblies = assemblies.Except(new[] {"mscorlib", "sysglobl"}).OrderBy(s => s);
-			foreach (var assembly in assemblies) {
-				Console.WriteLine(assembly);
-			}
+			var assemblies = new BclAssembliesProvider().GetBclAssemblies();
+
+			assemblies.ShouldContain("System.Data");
 			//also use AdditionalExplicitAssemblyReferences to add references and maybe TargetFrameworkDirectory
 		}
 	}
