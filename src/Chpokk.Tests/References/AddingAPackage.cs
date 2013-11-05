904d7f81f23b3693617d2801682d5b4628780939
Artem Smirnov <uluhonolulu@gmail.com>
2013-11-05 15:25:16 +0200

    package cache moved to production

--- src/Chpokk.Tests/References/AddingAPackage.cs
+++ src/Chpokk.Tests/References/AddingAPackage.cs
@@ -89,16 +89,4 @@ namespace Chpokk.Tests.References {
 			get { return Context.SolutionFolder.AppendPath("packages"); }
 		}
 	}
-
-	public class PackageInfoCache {
-		private readonly IDictionary<string, IPackage> _packages = new Dictionary<string, IPackage>();
-		public void Keep(IEnumerable<IPackage> packages) {
-			foreach (var package in packages) {
-				_packages[package.Id] = package;
-				Console.WriteLine("Adding " + package.Id);
-			}
-		}
-
-		public IPackage this[string id] { get { return _packages[id]; } }
-	}
 }
---
+++ src/ChpokkWeb/Features/ProjectManagement/References/NuGet/PackageInfoCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using NuGet;
+
+namespace ChpokkWeb.Features.ProjectManagement.References.NuGet {
+	public class PackageInfoCache {
+		private readonly IDictionary<string, IPackage> _packages = new Dictionary<string, IPackage>();
+		public void Keep(IEnumerable<IPackage> packages) {
+			foreach (var package in packages) {
+				_packages[package.Id] = package;
+				Console.WriteLine("Adding " + package.Id);
+			}
+		}
+
+		public IPackage this[string id] { get { return _packages[id]; } }
+	}
+}
