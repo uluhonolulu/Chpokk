2cb2a8920dc174fe18fdeab6a5a36bd8c2086561
Artem Smirnov <uluhonolulu@gmail.com>
2013-11-07 19:13:21 +0200

    Mailing BCL errors

--- src/ChpokkWeb/Features/ProjectManagement/References/Bcl/BclAssembliesProvider.cs
+++ src/ChpokkWeb/Features/ProjectManagement/References/Bcl/BclAssembliesProvider.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using Microsoft.Build.Construction;
 using Microsoft.Build.Evaluation;
 
 namespace ChpokkWeb.Features.ProjectManagement.References.Bcl {
 	public class BclAssembliesProvider {
 		private readonly IEnumerable<string> _assemblies;
+		private readonly SmtpClient _mailer;
 
-		public BclAssembliesProvider() {
+		public BclAssembliesProvider(SmtpClient mailer) {
+			_mailer = mailer;
 			var rootElement = ProjectRootElement.Create();
 			var targetImport = @"$(MSBuildToolsPath)\Microsoft.CSharp.targets";
 			rootElement.AddImport(targetImport);
@@ -26,6 +29,7 @@ namespace ChpokkWeb.Features.ProjectManagement.References.Bcl {
 				var message = "Invalid path: " + assemblyFolder;
 				var wrapper = new ApplicationException(message, exception);
 				Elmah.ErrorSignal.FromCurrentContext().Raise(wrapper);
+				if (_mailer.Host != null) _mailer.Send("features@chpokk.apphb.com", "uluhonolulu@gmail.com", "Assembly error", wrapper);
 				_assemblies = new string[]{};
 			}
 			
