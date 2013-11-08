2e5e0315b955acc7fd9ba8dcb3f39d150a5caf80
Artem Smirnov <uluhonolulu@gmail.com>
2013-11-08 14:01:50 +0200

    added a smoke container test

---
+++ src/SmokeTests/Bootstrapping.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChpokkWeb;
+using FubuMVC.Core;
+using FubuMVC.StructureMap;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using StructureMap;
+
+namespace SmokeTests {
+	[TestFixture]
+	public class Bootstrapping {
+		[Test]
+		public void CanGetAnInstance() {
+			var container = new Container();
+			FubuApplication.For<ConfigureFubuMVC>()
+				.StructureMap(container)
+				.Bootstrap();
+			//var engine = container.GetInstance<ILessEngine>();
+			Console.WriteLine(container.WhatDoIHave());
+
+		}
+	}
+}
