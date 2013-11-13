15033e97534d8cf3a7aaf7bd34fb03e69a0569d3
Artem Smirnov <uluhonolulu@gmail.com>
2013-11-13 20:26:02 +0200

    Removed SlickGrid; added Roslyn Console test

---
+++ src/RoslynVb/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynVb {
+	class Program {
+		static void Main(string[] args) {
+			var source = "Public Module Module1 \r\n Public Sub X() \r\n End Sub \r\n End Module";
+			var syntaxTree = Roslyn.Compilers.VisualBasic.SyntaxTree.ParseText(source);
+			var compilation = Roslyn.Compilers.VisualBasic.Compilation.Create("MyCompilation", syntaxTrees: new[] { syntaxTree });
+			var semanticModel = compilation.GetSemanticModel(syntaxTree);
+		}
+	}
+}
