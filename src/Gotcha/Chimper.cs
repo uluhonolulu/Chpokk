122a679dd7740d1084e2baba39439d61f80c1eab
Artem Smirnov <uluhonolulu@gmail.com>
2013-11-20 10:57:23 +0200

    Started working on MailChimp API

---
+++ src/Gotcha/Chimper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gotcha {
+	public class Chimper {
+		private string _apiKey = "c0ca38158b8a1c86d9f86421f6738c64-us2";
+		private const string _urlRoot = "https://us2.api.mailchimp.com/2.0/";
+
+		public void PrintListIDs() {
+			var url = _urlRoot + "/lists/list";
+			var data = new NameValueCollection() {{"apikey", _apiKey}};
+			using (var wc = new WebClient()){
+				wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+				var response = wc.UploadValues(url, WebRequestMethods.Http.Post, data);
+				var result = Encoding.ASCII.GetString(response);
+			}
+
+		}
+	}
+}
--- src/Gotcha/Import.cs
+++ src/Gotcha/Import.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Simple.Data;
 
 namespace Gotcha {
@@ -39,6 +40,25 @@ namespace Gotcha {
 				}
 			}
 			return default(DateTime);
+		}
+
+		public static void InsertUsers() {
+			var db = Database.Open();
+			var allUsages = db.Usages.All();
+			foreach (var usage in allUsages) {
+				var data = JsonConvert.DeserializeObject<dynamic>(usage.Body);
+				var profile = data.profile;
+				var email = profile.email;
+				var fullName = profile.displayName;
+				var userId = profile.preferredUsername + "_" + profile.providerName;
+				var photo = profile.photo;
+
+				var thisUser = db.Users.FindAllByUserId(userId.ToString()).FirstOrDefault();
+				if (thisUser == null) db.Users.Insert(Data: data, Email: email, FullName: fullName, UserId: userId, Photo: photo);
+
+				usage.UserId = userId.ToString();
+				db.Usages.Update(usage);
+			}
 		}
 	}
 }
