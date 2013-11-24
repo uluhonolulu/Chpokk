3c47c4c07e5c166889fb7e593a1a95755c223485
Artem Smirnov <uluhonolulu@gmail.com>
2013-11-24 19:35:24 +0200

    Fixed tests building

---
+++ src/Chpokk.Tests/Infrastructure/DirectoryHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpokk.Tests.Infrastructure {
+
+	public static class DirectoryHelper {
+		private static readonly Dictionary<string, string> toRename = new Dictionary<string, string>
+        {
+            { "dot_git", ".git" },
+            { "gitmodules", ".gitmodules" },
+        };
+
+		public static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target) {
+			// From http://stackoverflow.com/questions/58744/best-way-to-copy-the-entire-contents-of-a-directory-in-c/58779#58779
+
+			foreach (DirectoryInfo dir in source.GetDirectories()) {
+				CopyFilesRecursively(dir, target.CreateSubdirectory(Rename(dir.Name)));
+			}
+			foreach (FileInfo file in source.GetFiles()) {
+				file.CopyTo(Path.Combine(target.FullName, Rename(file.Name)));
+			}
+		}
+
+		private static string Rename(string name) {
+			return toRename.ContainsKey(name) ? toRename[name] : name;
+		}
+
+		public static void DeleteSubdirectories(string parentPath) {
+			string[] dirs = Directory.GetDirectories(parentPath);
+			foreach (string dir in dirs) {
+				DeleteDirectory(dir);
+			}
+		}
+
+		public static void DeleteDirectory(string directoryPath) {
+			// From http://stackoverflow.com/questions/329355/cannot-delete-directory-with-directory-deletepath-true/329502#329502
+
+			if (!Directory.Exists(directoryPath)) {
+				Trace.WriteLine(
+					string.Format("Directory '{0}' is missing and can't be removed.",
+						directoryPath));
+
+				return;
+			}
+
+			string[] files = Directory.GetFiles(directoryPath);
+			string[] dirs = Directory.GetDirectories(directoryPath);
+
+			foreach (string file in files) {
+				File.SetAttributes(file, FileAttributes.Normal);
+				File.Delete(file);
+			}
+
+			foreach (string dir in dirs) {
+				DeleteDirectory(dir);
+			}
+
+			File.SetAttributes(directoryPath, FileAttributes.Normal);
+			try {
+				Directory.Delete(directoryPath, false);
+			}
+			catch (IOException) {
+				Trace.WriteLine(string.Format("{0}The directory '{1}' could not be deleted!" +
+													"{0}Most of the time, this is due to an external process accessing the files in the temporary repositories created during the test runs, and keeping a handle on the directory, thus preventing the deletion of those files." +
+													"{0}Known and common causes include:" +
+													"{0}- Windows Search Indexer (go to the Indexing Options, in the Windows Control Panel, and exclude the bin folder of LibGit2Sharp.Tests)" +
+													"{0}- Antivirus (exclude the bin folder of LibGit2Sharp.Tests from the paths scanned by your real-time antivirus){0}",
+					Environment.NewLine, Path.GetFullPath(directoryPath)));
+			}
+		}
+	}
+}
