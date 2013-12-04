ac50f484d646bc0b402ce10abb88fe624a3ff6d7
Artem Smirnov <uluhonolulu@gmail.com>
2013-12-04 13:07:00 +0200

    spiking test fix

---
+++ src/Chpokk.Tests/Spikes/CanIDebugOrNot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Gallio.Framework;
+using Ivonna.Core;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using TypeMock;
+
+namespace Chpokk.Tests.Spikes {
+	[TestFixture]
+	public class CanIDebugOrNot {
+		[Test]
+		public void Test() {
+			TestMother.GetInstance(Path.GetFullPath(".."), "/", false, null);
+		}
+	}
+}
