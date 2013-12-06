33b2ec7d6a604a691becbc8d6acb3bb777f4f91e
Artem Smirnov <uluhonolulu@gmail.com>
2013-12-06 12:12:26 +0200

    New images, home cleanup

--- src/ChpokkWeb/Features/Demo/SuggestionModel.cs
+++
@@ -1,7 +0,0 @@
-namespace ChpokkWeb.Features.Demo {
-	public class SuggestionModel {
-		public string Message { get; set; }
-		public string Name { get; set; }
-		public string Email { get; set; }
-	}
-}
