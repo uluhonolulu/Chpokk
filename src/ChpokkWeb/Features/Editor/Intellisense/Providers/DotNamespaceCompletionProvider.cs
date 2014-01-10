daf9ac5d861ae588ff77412003390d75cb294c01
Artem Smirnov <uluhonolulu@gmail.com>
2014-01-10 10:02:32 +0200

    VBNet support for namespace completion

--- src/ChpokkWeb/Features/Editor/Intellisense/Providers/DotNamespaceCompletionProvider.cs
+++ src/ChpokkWeb/Features/Editor/Intellisense/Providers/DotNamespaceCompletionProvider.cs
@@ -7,9 +7,16 @@ namespace ChpokkWeb.Features.Editor.Intellisense.Providers {
 	public class DotNamespaceCompletionProvider {
 		public IEnumerable<IntelOutputModel.IntelModelItem> GetSymbols(CommonSyntaxToken token, ISemanticModel semanticModel, int position) {
 			if (token.ValueText == ".") {
-				var expressionSyntax = token.Parent as Roslyn.Compilers.CSharp.MemberAccessExpressionSyntax;
-				if (expressionSyntax != null) {
-					var symbolInfo = semanticModel.GetSymbolInfo(expressionSyntax.Expression);
+				var cSharpExpressionSyntax = token.Parent as Roslyn.Compilers.CSharp.MemberAccessExpressionSyntax;
+				if (cSharpExpressionSyntax != null) {
+					var symbolInfo = semanticModel.GetSymbolInfo(cSharpExpressionSyntax.Expression);
+					var symbol = symbolInfo.Symbol;
+					var lookupSymbols = semanticModel.LookupSymbols(position, symbol as INamespaceOrTypeSymbol);
+					return from lookupSymbol in lookupSymbols select IntelOutputModel.IntelModelItem.FromSymbol(lookupSymbol);
+				}
+				var vbNetExpressionSyntax = token.Parent as Roslyn.Compilers.VisualBasic.MemberAccessExpressionSyntax;
+				if (vbNetExpressionSyntax != null) {
+					var symbolInfo = semanticModel.GetSymbolInfo(vbNetExpressionSyntax.Expression);
 					var symbol = symbolInfo.Symbol;
 					var lookupSymbols = semanticModel.LookupSymbols(position, symbol as INamespaceOrTypeSymbol);
 					return from lookupSymbol in lookupSymbols select IntelOutputModel.IntelModelItem.FromSymbol(lookupSymbol);
