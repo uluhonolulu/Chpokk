9c2cd7de6a4fbb7a7ac4d578ae17d8599104ae0a
Artem Smirnov <uluhonolulu@gmail.com>
2014-01-10 10:26:55 +0200

    Fixed the this-dot intel case

--- src/ChpokkWeb/Features/Editor/Intellisense/Providers/DotNamespaceCompletionProvider.cs
+++ src/ChpokkWeb/Features/Editor/Intellisense/Providers/DotNamespaceCompletionProvider.cs
@@ -7,19 +7,22 @@ namespace ChpokkWeb.Features.Editor.Intellisense.Providers {
 	public class DotNamespaceCompletionProvider {
 		public IEnumerable<IntelOutputModel.IntelModelItem> GetSymbols(CommonSyntaxToken token, ISemanticModel semanticModel, int position) {
 			if (token.ValueText == ".") {
+				CommonSyntaxNode expression = null;
 				var cSharpExpressionSyntax = token.Parent as Roslyn.Compilers.CSharp.MemberAccessExpressionSyntax;
 				if (cSharpExpressionSyntax != null) {
-					var symbolInfo = semanticModel.GetSymbolInfo(cSharpExpressionSyntax.Expression);
-					var symbol = symbolInfo.Symbol;
-					var lookupSymbols = semanticModel.LookupSymbols(position, symbol as INamespaceOrTypeSymbol);
-					return from lookupSymbol in lookupSymbols select IntelOutputModel.IntelModelItem.FromSymbol(lookupSymbol);
+					expression = cSharpExpressionSyntax.Expression;
 				}
 				var vbNetExpressionSyntax = token.Parent as Roslyn.Compilers.VisualBasic.MemberAccessExpressionSyntax;
 				if (vbNetExpressionSyntax != null) {
-					var symbolInfo = semanticModel.GetSymbolInfo(vbNetExpressionSyntax.Expression);
-					var symbol = symbolInfo.Symbol;
-					var lookupSymbols = semanticModel.LookupSymbols(position, symbol as INamespaceOrTypeSymbol);
-					return from lookupSymbol in lookupSymbols select IntelOutputModel.IntelModelItem.FromSymbol(lookupSymbol);
+					expression = vbNetExpressionSyntax.Expression;
+				}
+				if (expression != null) {
+					var symbolInfo = semanticModel.GetSymbolInfo(expression);
+					var symbol = symbolInfo.Symbol as INamespaceOrTypeSymbol;
+					if (symbol != null) {
+						var lookupSymbols = semanticModel.LookupSymbols(position, symbol);
+						return from lookupSymbol in lookupSymbols select IntelOutputModel.IntelModelItem.FromSymbol(lookupSymbol);
+					}
 				}
 			}
 			return Enumerable.Empty<IntelOutputModel.IntelModelItem>();
