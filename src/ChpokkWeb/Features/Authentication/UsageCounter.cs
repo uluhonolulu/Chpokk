56d5ad86ccea00650ed456e8824fd28d5683bb33
Artem Smirnov <uluhonolulu@gmail.com>
2014-01-21 14:50:09 +0200

    Added usage counter to tracking mesages

---
+++ src/ChpokkWeb/Features/Authentication/UsageCounter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Simple.Data;
+
+namespace ChpokkWeb.Features.Authentication {
+	public class UsageCounter {
+		public int GetUsageCount(string userName) {
+			var db = Database.Open();
+			return db.Usages.GetCountByUserId(userName);
+		}
+	}
+}
--- src/ChpokkWeb/Features/CustomerDevelopment/ActivityTracker.cs
+++ src/ChpokkWeb/Features/CustomerDevelopment/ActivityTracker.cs
@@ -13,11 +13,13 @@ namespace ChpokkWeb.Features.CustomerDevelopment {
 		private readonly List<ITrack> _log = new List<ITrack>();
 		private readonly SmtpClient _mailer;
 		private readonly UsageRecorder _usageRecorder;
+		private readonly UsageCounter _usageCounter;
 		private readonly ISecurityContext _securityContext;
-		public ActivityTracker(SmtpClient mailer, UsageRecorder usageRecorder, ISecurityContext securityContext) {
+		public ActivityTracker(SmtpClient mailer, UsageRecorder usageRecorder, ISecurityContext securityContext, UsageCounter usageCounter) {
 			_mailer = mailer;
 			_usageRecorder = usageRecorder;
 			_securityContext = securityContext;
+			_usageCounter = usageCounter;
 		}
 
 
@@ -36,18 +38,27 @@ namespace ChpokkWeb.Features.CustomerDevelopment {
 				messageBuilder.AppendLine(model.ToString());
 			}
 			if (_mailer.Host != null) {
-				var subject = "Actions for " + UserName;
+				var subject = GetSubject();
 				var body = messageBuilder.ToString();
-				if (body.Contains("startTrial")) {
-					subject += " (started trial!!!)";
-				}
-				if (body.Contains("cancelTrial")) {
-					subject += " (canceled trial!!!)";
-				}
 				_mailer.Send("actions@chpokk.apphb.com", "uluhonolulu@gmail.com", subject, body);
 			}
 
 			_usageRecorder.AddUsage(UserName, messageBuilder.ToString());
+		}
+
+		private string GetSubject() {
+			var subject = "Actions for " + UserName;
+			var previousUsages = _usageCounter.GetUsageCount(UserName);
+			subject += ", previous usages: {0}.".ToFormat(previousUsages);
+			if (HasThisAction("startTrial")) subject += " (started trial!!!)";
+			if (HasThisAction("cancelTrial")) subject += " (canceled trial!!!)";
+			if (HasThisAction("createSimpleProjectButton")) subject += " (created a project)";
+			if (HasThisAction("http://chpokk.apphb.com/Repository/")) subject += " (opened the editor)";
+			return subject;
+		}
+
+		private bool HasThisAction(string searchString) {
+			return _log.Select(track => track.ToString()).Any(s => s.Contains(searchString));
 		}
 
 		public string UserName { get; set; }
