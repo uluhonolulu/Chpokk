67fad2470117341b5c7510c575a175fa789245b7
Artem Smirnov <uluhonolulu@gmail.com>
2014-02-01 18:04:23 +0200

    bitness

---
+++ src/SmokeTests/Bitness.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SmokeTests {
+	public class Bitness {
+		[Test]
+		public void CheckBitness() {
+			Console.WriteLine(Environment.Is64BitProcess);
+			Console.WriteLine("=========================================================================================");
+		}
+	}
+}
