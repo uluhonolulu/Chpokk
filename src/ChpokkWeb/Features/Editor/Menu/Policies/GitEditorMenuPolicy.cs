5083e69e875ee4b0d9bcf393abded0d454dd8ca6
Artem Smirnov <uluhonolulu@gmail.com>
2014-02-04 13:31:39 +0200

    git menu policy

---
+++ src/ChpokkWeb/Features/Editor/Menu/Policies/GitEditorMenuPolicy.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using ChpokkWeb.Features.RepositoryManagement;
+
+namespace ChpokkWeb.Features.Editor.Menu.Policies {
+
+	public class GitEditorMenuPolicy : VersionControlEditorMenuPolicy {
+		private readonly RepositoryManager _repositoryManager;
+		public GitEditorMenuPolicy(RepositoryManager repositoryManager) {
+			_repositoryManager = repositoryManager;
+		}
+
+		public override bool Matches(RepositoryInfo info, string approot) {
+			var path = _repositoryManager.NewGetAbsolutePathFor(info.Name, ".git");
+			return Directory.Exists(path);
+		}
+
+
+	}
+}
