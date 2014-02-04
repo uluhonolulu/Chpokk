9ebc97ed5ccc6210a19845c65e7d6696f7c4b12d
Artem Smirnov <uluhonolulu@gmail.com>
2014-02-04 13:27:37 +0200

    Now we see the commit menu item for svn repos

---
+++ src/ChpokkWeb/Features/Editor/Menu/Policies/SvnEditorMenuPolicy.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using ChpokkWeb.Features.RepositoryManagement;
+
+namespace ChpokkWeb.Features.Editor.Menu.Policies {
+	public class SvnEditorMenuPolicy: VersionControlEditorMenuPolicy {
+		private readonly RepositoryManager _repositoryManager;
+		public SvnEditorMenuPolicy(RepositoryManager repositoryManager) {
+			_repositoryManager = repositoryManager;
+		}
+
+		public override bool Matches(RepositoryInfo info, string approot) {
+			var path = _repositoryManager.NewGetAbsolutePathFor(info.Name, ".svn");
+			return Directory.Exists(path);
+		}
+	}
+}
