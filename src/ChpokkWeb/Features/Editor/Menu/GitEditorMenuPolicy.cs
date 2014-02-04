fa9b7dde802fa7539155643985cf45609b4a56a5
Artem Smirnov <uluhonolulu@gmail.com>
2014-02-04 13:05:25 +0200

    Introduced Version Control Editor Menu Policy

--- src/ChpokkWeb/Features/Editor/Menu/GitEditorMenuPolicy.cs
+++ src/ChpokkWeb/Features/Editor/Menu/GitEditorMenuPolicy.cs
@@ -8,15 +8,12 @@ using FubuCore;
 using FubuMVC.Navigation;
 
 namespace ChpokkWeb.Features.RepositoryManagement.Git {
-	public class GitEditorMenuPolicy: IEditorMenuPolicy {
-		public bool Matches(RepositoryInfo info, string approot) {
-			var stoff = FubuCore.FileSystem.Combine("C:\\", info.Path);
+	public class GitEditorMenuPolicy : VersionControlEditorMenuPolicy {
+		public override bool Matches(RepositoryInfo info, string approot) {
 			var path = FubuCore.FileSystem.Combine(approot, info.Path, ".git");
 			return Directory.Exists(path);
 		}
 
-		public IEnumerable<MenuItemToken> GetMenuItems() {
-			yield return new MenuItemToken(){Text = "Save and Commit", Key = "saveCommit", MenuItemState = MenuItemState.Available};
-		}
+
 	}
 }
---
+++ src/ChpokkWeb/Features/Editor/Menu/VersionControlEditorMenuPolicy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChpokkWeb.Features.RepositoryManagement;
+using FubuMVC.Navigation;
+
+namespace ChpokkWeb.Features.Editor.Menu {
+	public abstract class VersionControlEditorMenuPolicy : IEditorMenuPolicy {
+		public abstract bool Matches(RepositoryInfo info, string approot);
+
+		public IEnumerable<MenuItemToken> GetMenuItems() {
+			yield return new MenuItemToken() { Text = "Save and Commit", Key = "saveCommit", MenuItemState = MenuItemState.Available };
+		}
+	}
+}
