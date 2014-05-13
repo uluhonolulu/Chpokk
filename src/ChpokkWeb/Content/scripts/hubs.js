0ef4d664c1e3bfecb9275be06af2279f38e7faa5
Artem Smirnov <uluhonolulu@gmail.com>
2014-05-13 12:46:52 +0300

    Updated the hubs script

--- src/ChpokkWeb/Content/scripts/hubs.js
+++ src/ChpokkWeb/Content/scripts/hubs.js
@@ -11,110 +11,95 @@
 /// <reference path="..\..\SignalR.Client.JS\Scripts\jquery-1.6.4.js" />
 /// <reference path="jquery.signalR.js" />
 (function ($, window, undefined) {
-    /// <param name="$" type="jQuery" />
-    "use strict";
+	/// <param name="$" type="jQuery" />
+	"use strict";
 
-    if (typeof ($.signalR) !== "function") {
-        throw new Error("SignalR: SignalR is not loaded. Please ensure jquery.signalR-x.js is referenced before ~/signalr/js.");
-    }
+	if (typeof ($.signalR) !== "function") {
+		throw new Error("SignalR: SignalR is not loaded. Please ensure jquery.signalR-x.js is referenced before ~/signalr/js.");
+	}
 
-    var signalR = $.signalR;
+	var signalR = $.signalR;
 
-    function makeProxyCallback(hub, callback) {
-        return function () {
-            // Call the client hub method
-            callback.apply(hub, $.makeArray(arguments));
-        };
-    }
+	function makeProxyCallback(hub, callback) {
+		return function () {
+			// Call the client hub method
+			callback.apply(hub, $.makeArray(arguments));
+		};
+	}
 
-    function registerHubProxies(instance, shouldSubscribe) {
-        var key, hub, memberKey, memberValue, subscriptionMethod;
+	function registerHubProxies(instance, shouldSubscribe) {
+		var key, hub, memberKey, memberValue, subscriptionMethod;
 
-        for (key in instance) {
-            if (instance.hasOwnProperty(key)) {
-                hub = instance[key];
+		for (key in instance) {
+			if (instance.hasOwnProperty(key)) {
+				hub = instance[key];
 
-                if (!(hub.hubName)) {
-                    // Not a client hub
-                    continue;
-                }
+				if (!(hub.hubName)) {
+					// Not a client hub
+					continue;
+				}
 
-                if (shouldSubscribe) {
-                    // We want to subscribe to the hub events
-                    subscriptionMethod = hub.on;
-                } else {
-                    // We want to unsubscribe from the hub events
-                    subscriptionMethod = hub.off;
-                }
+				if (shouldSubscribe) {
+					// We want to subscribe to the hub events
+					subscriptionMethod = hub.on;
+				} else {
+					// We want to unsubscribe from the hub events
+					subscriptionMethod = hub.off;
+				}
 
-                // Loop through all members on the hub and find client hub functions to subscribe/unsubscribe
-                for (memberKey in hub.client) {
-                    if (hub.client.hasOwnProperty(memberKey)) {
-                        memberValue = hub.client[memberKey];
+				// Loop through all members on the hub and find client hub functions to subscribe/unsubscribe
+				for (memberKey in hub.client) {
+					if (hub.client.hasOwnProperty(memberKey)) {
+						memberValue = hub.client[memberKey];
 
-                        if (!$.isFunction(memberValue)) {
-                            // Not a client hub function
-                            continue;
-                        }
+						if (!$.isFunction(memberValue)) {
+							// Not a client hub function
+							continue;
+						}
 
-                        subscriptionMethod.call(hub, memberKey, makeProxyCallback(hub, memberValue));
-                    }
-                }
-            }
-        }
-    }
+						subscriptionMethod.call(hub, memberKey, makeProxyCallback(hub, memberValue));
+					}
+				}
+			}
+		}
+	}
 
-    $.hubConnection.prototype.createHubProxies = function () {
-        var proxies = {};
-        this.starting(function () {
-            // Register the hub proxies as subscribed
-            // (instance, shouldSubscribe)
-            registerHubProxies(proxies, true);
+	$.hubConnection.prototype.createHubProxies = function () {
+		var proxies = {};
+		this.starting(function () {
+			// Register the hub proxies as subscribed
+			// (instance, shouldSubscribe)
+			registerHubProxies(proxies, true);
 
-            this._registerSubscribedHubs();
-        }).disconnected(function () {
-            // Unsubscribe all hub proxies when we "disconnect".  This is to ensure that we do not re-add functional call backs.
-            // (instance, shouldSubscribe)
-            registerHubProxies(proxies, false);
-        });
+			this._registerSubscribedHubs();
+		}).disconnected(function () {
+			// Unsubscribe all hub proxies when we "disconnect".  This is to ensure that we do not re-add functional call backs.
+			// (instance, shouldSubscribe)
+			registerHubProxies(proxies, false);
+		});
 
-        proxies.compilerHub = this.createHubProxy('compilerHub');
-        proxies.compilerHub.client = {};
-        proxies.compilerHub.server = {
-            compile: function (model) {
-                return proxies.compilerHub.invoke.apply(proxies.compilerHub, $.merge(["Compile"], $.makeArray(arguments)));
-            },
+		proxies.compilerHub = this.createHubProxy('compilerHub');
+		proxies.compilerHub.client = {};
+		proxies.compilerHub.server = {
+		};
 
-            compileAndRun: function (model) {
-                return proxies.compilerHub.invoke.apply(proxies.compilerHub, $.merge(["CompileAndRun"], $.makeArray(arguments)));
-            },
+		proxies.nuGetHub = this.createHubProxy('nuGetHub');
+		proxies.nuGetHub.client = {};
+		proxies.nuGetHub.server = {
+		};
 
-            initialize: function (eventSource) {
-                return proxies.compilerHub.invoke.apply(proxies.compilerHub, $.merge(["Initialize"], $.makeArray(arguments)));
-            },
+		proxies.userHub = this.createHubProxy('userHub');
+		proxies.userHub.client = {};
+		proxies.userHub.server = {
+			onStartPlayingWith: function () {
+				return proxies.userHub.invoke.apply(proxies.userHub, $.merge(["OnStartPlayingWith"], $.makeArray(arguments)));
+			}
+		};
 
-            shutdown: function () {
-                return proxies.compilerHub.invoke.apply(proxies.compilerHub, $.merge(["Shutdown"], $.makeArray(arguments)));
-            }
-        };
+		return proxies;
+	};
 
-        proxies.nuGetHub = this.createHubProxy('nuGetHub');
-        proxies.nuGetHub.client = {};
-        proxies.nuGetHub.server = {
-        };
-
-        proxies.userHub = this.createHubProxy('userHub');
-        proxies.userHub.client = {};
-        proxies.userHub.server = {
-            onStartPlayingWith: function () {
-                return proxies.userHub.invoke.apply(proxies.userHub, $.merge(["OnStartPlayingWith"], $.makeArray(arguments)));
-            }
-        };
-
-        return proxies;
-    };
-
-    signalR.hub = $.hubConnection("/signalr", { useDefaultPath: false });
-    $.extend(signalR, signalR.hub.createHubProxies());
+	signalR.hub = $.hubConnection("/signalr", { useDefaultPath: false });
+	$.extend(signalR, signalR.hub.createHubProxies());
 
 }(window.jQuery, window));
