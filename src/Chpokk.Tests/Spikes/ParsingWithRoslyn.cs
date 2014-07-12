924d3028f99e7e5026d27f068dec46070ebe6ba4
Artem Smirnov <uluhonolulu@gmail.com>
2014-07-12 14:29:39 +0300

    Parsing now uses Roslyn

---
+++ src/Chpokk.Tests/Spikes/ParsingWithRoslyn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using Roslyn.Compilers.Common;
+
+namespace Chpokk.Tests.Spikes {
+	[TestFixture]
+	public class ParsingWithRoslyn {
+		[Test]
+		public void Test() {
+			var source = @"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class Program {
+	static void Main(string[] args) {
+		Console.WriteLine(""Hi!"");
+	}
+//}
+";
+			CommonSyntaxTree tree;
+			tree = Roslyn.Compilers.CSharp.SyntaxTree.ParseText(source);
+			var diagnostics = tree.GetRoot().GetDiagnostics();
+			foreach (var diagnostic in diagnostics) {
+				Console.WriteLine(diagnostic.ToString());
+				Console.WriteLine(diagnostic.Info);
+				Console.WriteLine(diagnostic.Location.GetLineSpan(true));
+			}
+		}
+	}
+}
--- src/ChpokkWeb/Features/Editor/Parsing/ParserController.cs
+++ src/ChpokkWeb/Features/Editor/Parsing/ParserController.cs
@@ -1,7 +1,10 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using ChpokkWeb.Features.LanguageSupport;
 using FubuMVC.Core.Ajax;
 using ICSharpCode.NRefactory;
+using Roslyn.Compilers.Common;
 
 // use ICSharpCode.NRefactory.ParserFactory to create a parser based on filename
 
@@ -12,19 +15,24 @@ namespace ChpokkWeb.Features.Editor.Parsing {
 			_languageDetector = languageDetector;
 		}
 
-		public AjaxContinuation Parse(ParserInputModel input) {
+		public ParserModel Parse(ParserInputModel input) {
 			//var filePath = _repositoryManager.GetPhysicalFilePath(input);
+			CommonSyntaxTree tree;
 			var language = _languageDetector.GetLanguage(input.PathRelativeToRepositoryRoot);
-			var parser = ParserFactory.CreateParser(language, new StringReader(input.Content));
-			parser.Parse();
-			if (parser.Errors.Count == 0) {
-				return AjaxContinuation.Successful();
-			}
-			else {
-				var continuation = new AjaxContinuation();
-				continuation.Errors.Add(new AjaxError {message = parser.Errors.ErrorOutput});
-				return continuation;
-			}
+			tree = language==SupportedLanguage.CSharp? (CommonSyntaxTree) Roslyn.Compilers.CSharp.SyntaxTree.ParseText(input.Content) : Roslyn.Compilers.VisualBasic.SyntaxTree.ParseText(input.Content);
+			var diagnostics = tree.GetRoot().GetDiagnostics();
+			var errors = from diagnostic in diagnostics
+			             select
+				             new ErrorInfo()
+					             {
+						             Message = diagnostic.Info.ToString(),
+						             PositionSpan = diagnostic.Location.GetLineSpan(false)
+					             };
+			return new ParserModel() {Errors = errors};
 		}
+	}
+
+	public class ParserModel {
+		public IEnumerable<ErrorInfo> Errors { get; set; }
 	}
 }
