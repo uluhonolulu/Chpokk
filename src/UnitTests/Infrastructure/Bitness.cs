5ea5f3b221c1c1f83493d78e74ba06ee6e102c29
Artem Smirnov <uluhonolulu@gmail.com>
2014-09-06 12:16:07 +0300

    UnitTests to x86 again

---
+++ src/UnitTests/Infrastructure/Bitness.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests.Infrastructure {
+	public class Bitness {
+		[Test]
+		public void CheckBitness() {
+			Console.WriteLine("Is64BitProcess " + Environment.Is64BitProcess);
+			Console.WriteLine("=========================================================================================");
+		}
+	}
+}
