90e38b4b78dfdddea0b5d5a7b9e18f3c79d8e79c
Artem Smirnov <uluhonolulu@gmail.com>
2014-09-07 14:03:36 +0300

    js refactoring for adding a project

---
+++ src/ChpokkWeb/Features/ProjectManagement/Properties/ProjectPropertiesEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChpokkWeb.Features.ProjectManagement.Properties {
+	public class ProjectPropertiesEndpoint {
+		public ProjectPropertiesModel DoIt(ProjectPropertiesInputModel model) {
+			var output = new ProjectPropertiesModel();
+			output.BclReferences.Add("System", false);
+			output.BclReferences.Add("System.Core", true);
+			output.PackageReferences.Add("Autofac");
+			return output;
+		}
+	}
+
+	public class ProjectPropertiesModel {
+		public IDictionary<string, bool> BclReferences = new Dictionary<string, bool>();
+		public IList<string> PackageReferences = new List<string>();
+		public IDictionary<string, bool> ProjectReferences = new Dictionary<string, bool>();
+	}
+
+	public class ProjectPropertiesInputModel {}
+}
