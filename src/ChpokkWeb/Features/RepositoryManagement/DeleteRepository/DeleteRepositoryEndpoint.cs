23df26f90d57bbf7b88eae641330dcb29b5100a3
Artem Smirnov <uluhonolulu@gmail.com>
2014-09-18 11:55:56 +0300

    Deleting a repo, server code

---
+++ src/ChpokkWeb/Features/RepositoryManagement/DeleteRepository/DeleteRepositoryEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChpokkWeb.Features.Exploring;
+using ChpokkWeb.Features.MainScreen;
+using FubuCore;
+using FubuMVC.Core.Ajax;
+using FubuMVC.Core.Urls;
+using ChpokkWeb.Infrastructure;
+
+namespace ChpokkWeb.Features.RepositoryManagement.DeleteRepository {
+	public class DeleteRepositoryEndpoint {
+		private readonly RepositoryManager _repositoryManager;
+		private readonly UrlRegistry _urlRegistry;
+		private readonly IFileSystem _fileSystem;
+		public DeleteRepositoryEndpoint(RepositoryManager repositoryManager, UrlRegistry urlRegistry, IFileSystem fileSystem) {
+			_repositoryManager = repositoryManager;
+			_urlRegistry = urlRegistry;
+			_fileSystem = fileSystem;
+		}
+
+		public AjaxContinuation DoIt(DeleteRepositoryInputModel model) {
+			var path = _repositoryManager.NewGetAbsolutePathFor(model.RepositoryName);
+			_fileSystem.DeleteDirectory(path);
+			var url = _urlRegistry.UrlFor<MainDummyModel>();
+			return new AjaxContinuation().NavigateTo(url);
+		}
+	}
+
+	public class DeleteRepositoryInputModel: BaseRepositoryInputModel {}
+}
