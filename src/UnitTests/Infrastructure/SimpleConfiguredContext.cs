cf254af5d5a61dcf918db889805da75a402d1909
Artem Smirnov <uluhonolulu@gmail.com>
2014-09-20 12:05:55 +0300

    Deep search for content

--- src/UnitTests/Infrastructure/SimpleConfiguredContext.cs
+++ src/UnitTests/Infrastructure/SimpleConfiguredContext.cs
@@ -69,12 +69,12 @@ namespace UnitTests.Infrastructure {
 			if (str.EndsWith("bin"))
 				str = str.Substring(0, str.Length - 3).TrimEnd(Path.DirectorySeparatorChar);
 			Console.WriteLine("FubuMvcPackageFacility.determineApplicationPathFromAppDomain(): " + str);
-			var contentFolder = Directory.GetDirectories(str, "*Content").FirstOrDefault();
+			var contentFolder = Directory.GetDirectories(str, "content", SearchOption.AllDirectories).FirstOrDefault(s => !s.Contains("fubu-content"));
 			if (contentFolder != null) {
 				Console.WriteLine("Found content at " + contentFolder);
 			}
 			Console.WriteLine("Current folder: " + Directory.GetCurrentDirectory());
-			FubuMvcPackageFacility.PhysicalRootPath = str;
+			FubuMvcPackageFacility.PhysicalRootPath = contentFolder.ParentDirectory();
 
 			_container = new Container();
 			ConfigureContainer(_container);
