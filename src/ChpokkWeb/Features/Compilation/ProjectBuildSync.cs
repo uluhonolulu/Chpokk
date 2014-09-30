416d7a3a347767b6cd4e6e809e8e306f084e1528
Artem Smirnov <uluhonolulu@gmail.com>
2014-09-30 22:25:04 +0300

    Added ProjectBuildSync

---
+++ src/ChpokkWeb/Features/Compilation/ProjectBuildSync.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Build.Evaluation;
+
+namespace ChpokkWeb.Features.Compilation {
+	static class ProjectBuildSync {
+		private static readonly object _syncObject = new object();
+		public static bool Build(Project project) {
+			lock (_syncObject)
+				return project.Build();
+		}
+	}
+
+}
