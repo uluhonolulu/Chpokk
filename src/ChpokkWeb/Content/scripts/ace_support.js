fecddaa79a40395dbae16cb3aa6f3350feb66f29
Artem Smirnov <uluhonolulu@gmail.com>
2014-10-01 10:57:09 +0300

    Switching autosuggest off

--- src/ChpokkWeb/Content/scripts/ace_support.js
+++ src/ChpokkWeb/Content/scripts/ace_support.js
@@ -1,12 +1,12 @@
 ﻿$(function () {
-    window.define = ace.define;
-    ace.require("ace/ext/language_tools");
+	window.define = ace.define;
+	ace.require("ace/ext/language_tools");
 	var editor = ace.edit("ace");
 	editor.setTheme("ace/theme/idle_fingers");
-    
-    //bug: .ace_content too narrow in IE
-    
-    //completion
+	
+	//bug: .ace_content too narrow in IE
+	
+	//completion
 	var codeCompleter = {
 		getCompletions: function (editor, session, pos, prefix, callback) {
 			if ($('.ace_autocomplete').is(':visible')) { 
@@ -23,26 +23,26 @@
 			var model = tabs.activeModel();
 			var editorData = $.extend({}, model, { Content: text, NewChar: newChar, Position: position }); 
 			$.post('url::ChpokkWeb.Features.Editor.Intellisense.IntelInputModel', editorData, function (data) {
-			    var matchingItems = $.grep(data.Items, function(item) {
-			        return item.Name.startsWithIgnoreCase(prefix);
-			    });
-			    var completionData = $.map(matchingItems, function (item, index) {
-			        return {
-			            name: item.Name,
-			            value: item.Name + (item.EntityType == 'Method' ? '()' : ''),
-			            caption: item.Name,
-			            className: 'completion_' + item.EntityType + ' completion',
-			            score: data.Items.length - index
-			        };//TODO: .completer.insertMatch(this.editor); or if (data.snippet) snippetManager.insertSnippet(this.editor, data.snippet);
+				var matchingItems = $.grep(data.Items, function(item) {
+					return item.Name.startsWithIgnoreCase(prefix);
 				});
-			    callback(null, completionData);
+				var completionData = $.map(matchingItems, function (item, index) {
+					return {
+						name: item.Name,
+						value: item.Name + (item.EntityType == 'Method' ? '()' : ''),
+						caption: item.Name,
+						className: 'completion_' + item.EntityType + ' completion',
+						score: data.Items.length - index
+					};//TODO: .completer.insertMatch(this.editor); or if (data.snippet) snippetManager.insertSnippet(this.editor, data.snippet);
+				});
+				callback(null, completionData);
 				//IE fix
-			    if (editor.completer.popup) {
-			        var renderer = editor.completer.popup.renderer;
-			        renderer.desiredHeight = undefined;
-			        renderer.onResize(true);
-			        //renderer.$textLayer.checkForSizeChanges();
-			    }
+				if (editor.completer.popup) {
+					var renderer = editor.completer.popup.renderer;
+					renderer.desiredHeight = undefined;
+					renderer.onResize(true);
+					//renderer.$textLayer.checkForSizeChanges();
+				}
 
 			});
 		}
@@ -54,36 +54,41 @@
 	});
 	editor.completers = [codeCompleter];
 
-    // fire autocomplete on any char
+	// fire autocomplete on any char
 	$('#ace').keypress(function (e) {
-	    if (editor.enableIntellisense) {
-		    var char = String.fromCharCode(e.which);
-		    var regexp = /[a-zA-Z_0-9\.]/;
-		    if (regexp.test(char)) {
-			    $('#ace').one('keyup', function () {
-				    editor.commands.exec('startAutocomplete', editor); //temporarily disable the autocomplete
-			    });
-		    }
-		    
-	    	//on enter, check syntax
-		    if (e.which === $.ui.keyCode.ENTER) {
-			    checkSyntax(editor);
-		    }
-        }
-	});        
+		if (editor.enableIntellisense) {
+			var char = String.fromCharCode(e.which);
+			var regexp = /[a-zA-Z_0-9\.]/;
+			if (regexp.test(char)) {
+				$('#ace').one('keyup', function () {
+					editor.commands.exec('startAutocomplete', editor); //temporarily disable the autocomplete
+				});
+			}
+			
+			//on enter, check syntax
+			if (e.which === $.ui.keyCode.ENTER) {
+				checkSyntax(editor);
+			}
+		}
+	});
+	
+	// enable/disable autocompletion
+	$('#autoSuggestOff').click(function() {
+		editor.enableIntellisense = !$(this).is(':checked');
+	});
 
-    
+
 	//editor.completers = [snippetCompleter, textCompleter, keyWordCompleter];
 });
 
 function getPosition(rowColumn, editor) {
-    var lines = editor.session.doc.getAllLines();
-    var text = editor.getValue();
+	var lines = editor.session.doc.getAllLines();
+	var text = editor.getValue();
 	var position = 0;
 	for (var row = 0; row < rowColumn.row; row++) {
-	    //we set the position to the start of the (row+1)th line
-	    //we look for the start of that line, starting from the end of the row-th line
-	    position = text.indexOf(lines[row + 1], position + lines[row].length);
+		//we set the position to the start of the (row+1)th line
+		//we look for the start of that line, starting from the end of the row-th line
+		position = text.indexOf(lines[row + 1], position + lines[row].length);
 	}
 	position += rowColumn.column;
 	return position;
@@ -112,7 +117,7 @@ window.tabs = window.tabs || {
 window.tabs.all = window.tabs.all || {};
 
 function loadFile(path, editor, onload) {
-    $('#fileContent').show();
+	$('#fileContent').show();
 	var selector = 'li[data-path="' + path.replace(/\\/g, '\\\\') + '"]';
 	var itemContainer = $('#solutionBrowser ' + selector + ' .file');
 	var fileData = $.extend({}, model, itemContainer.data());
