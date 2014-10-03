fc92a4a4de8d6725d528df70f5ed4f062e423418
Artem Smirnov <uluhonolulu@gmail.com>
2014-10-03 16:00:59 +0300

    Uploading assembly to a hardcoded path

---
+++ src/ChpokkWeb/Features/ProjectManagement/References/Files/UploadAssemblyEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FubuCore;
+
+namespace ChpokkWeb.Features.ProjectManagement.References.Files {
+	public class UploadAssemblyEndpoint {
+		public void DoIt(UploadAssemblyInputModel model) {
+			if (model.Assembly == null) {
+				return;
+			}
+			model.Assembly.SaveAs(@"D:\Projects\Chpokk\src\ChpokkWeb\UserFiles\uluhonolulu_Google\Repositories\sim1".AppendPath(model.Assembly.FileName));
+		}
+	}
+
+	public class UploadAssemblyInputModel {
+		public HttpPostedFileBase Assembly { get; set; }
+	}
+}
