77376fa9f236427b1f2d3e86543dec2b3077fe03
Artem Smirnov <uluhonolulu@gmail.com>
2014-10-20 15:21:52 +0300

    output props

--- src/ChpokkWeb/Features/Compilation/MsBuildCompiler.cs
+++ src/ChpokkWeb/Features/Compilation/MsBuildCompiler.cs
@@ -32,10 +32,10 @@ namespace ChpokkWeb.Features.Compilation {
 			}
 			var importPath = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\Microsoft.CSharp.targets";
 			Console.WriteLine(importPath + ": " + File.Exists(importPath));
-			//Console.WriteLine("PROPS");
-			//foreach (var property in project.AllEvaluatedProperties) {
-			//	Console.WriteLine(property.Name + ": " + property.EvaluatedValue);
-			//}
+			Console.WriteLine("PROPS");
+			foreach (var property in project.AllEvaluatedProperties) {
+				Console.WriteLine(property.Name + ": " + property.EvaluatedValue);
+			}
 			var buildResult = ProjectBuildSync.Build(project, logger);
 			var outputPathProperty = project.AllEvaluatedProperties.First(property => property.Name == "OutputPath");
 			var targetProperty = project.AllEvaluatedProperties.First(property => property.Name == "TargetFileName");
