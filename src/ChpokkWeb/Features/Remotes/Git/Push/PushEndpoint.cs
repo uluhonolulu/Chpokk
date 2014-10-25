8b5fbdabdda2a942895804be13710a46e0df3cd4
Artem Smirnov <uluhonolulu@gmail.com>
2014-10-25 16:13:35 +0300

    GitHaub renamed to Git, PushController renamed to PushEndpoint

--- src/ChpokkWeb/Features/Remotes/Git/Push/PushEndpoint.cs
+++ src/ChpokkWeb/Features/Remotes/Git/Push/PushEndpoint.cs
@@ -4,9 +4,9 @@ using FubuMVC.Core.Ajax;
 using LibGit2Sharp;
 
 namespace ChpokkWeb.Features.Remotes.Git.Push {
-	public class PushController {
-		private RepositoryManager _manager;
-		public PushController(RepositoryManager manager) {
+	public class PushEndpoint {
+		private readonly RepositoryManager _manager;
+		public PushEndpoint(RepositoryManager manager) {
 			_manager = manager;
 		}
 
@@ -14,13 +14,12 @@ namespace ChpokkWeb.Features.Remotes.Git.Push {
 		public AjaxContinuation Push(PushInputModel model) {
 			var credentials = model.Username.IsEmpty()? null: new LibGit2Sharp.Credentials {Username = model.Username, Password = model.Password};
 			var path = _manager.NewGetAbsolutePathFor(model.RepositoryName);
-			string errorMessage;
 			var ajaxContinuation = AjaxContinuation.Successful();
 			using (var repo = new Repository(path)) {
 				var remote = repo.Network.Remotes["origin"];
 				repo.Network.Push(remote, "refs/heads/master", error => {
 					ajaxContinuation.Success = false;
-					errorMessage = error.Reference + ": " + error.Message + "/r";
+					var errorMessage = error.Reference + ": " + error.Message + "/r";
 					ajaxContinuation.Errors.Add(new AjaxError { message = errorMessage });
 				}, credentials);
 			}
