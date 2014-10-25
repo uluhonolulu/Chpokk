ab1b0343e4cc474e2ade9b42ac8074eb41a2cae2
Artem Smirnov <uluhonolulu@gmail.com>
2014-10-25 12:20:26 +0300

    Moved push script to push dialog

--- src/ChpokkWeb/Features/Remotes/Git/Init/GitInitPolicy.cs
+++ src/ChpokkWeb/Features/Remotes/Git/Init/GitInitPolicy.cs
@@ -1,9 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using ChpokkWeb.Features.RepositoryManagement;
+using ChpokkWeb.Infrastructure;
+using FubuCore;
 
 namespace ChpokkWeb.Features.Remotes.Git.Init {
-	public class GitInitPolicy: IMu {
+	public class GitInitPolicy: IRetrievePolicy, IMenuItemSource {
+		public bool Matches(RepositoryInfo info, string approot) {
+			//match when we don't have a .git folder
+			var path = FileSystem.Combine(approot, info.Path, ".git");
+			return !Directory.Exists(path);
+		}
+
+		public MenuItem GetMenuItem() {
+			return new MenuItem(){Caption = "Git init", Id = "gitinit"};
+		}
 	}
 }
