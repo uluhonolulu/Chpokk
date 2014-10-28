5ea2eeeed51da83e181a1aea67cb2833ca170c58
Artem Smirnov <uluhonolulu@gmail.com>
2014-10-28 20:53:15 +0200

    Opening advanced push dialog

--- src/ChpokkWeb/Features/Remotes/Git/Push/Advanced/AdvancedPushPolicy.cs
+++ src/ChpokkWeb/Features/Remotes/Git/Push/Advanced/AdvancedPushPolicy.cs
@@ -2,8 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ChpokkWeb.Features.Remotes.Git.Init;
+using ChpokkWeb.Infrastructure;
+using ChpokkWeb.Infrastructure.Menu;
 
 namespace ChpokkWeb.Features.Remotes.Git.Push.Advanced {
-	public class AdvancedPushPolicy {
+	public class AdvancedPushPolicy: IRetrievePolicy, IMenuItemSource {
+		private readonly GitInitializer _gitInitializer;
+		public AdvancedPushPolicy(GitInitializer gitInitializer) {
+			_gitInitializer = gitInitializer;
+		}
+
+		public bool Matches(string repositoryRoot) {
+			return _gitInitializer.GitRepositoryExistsIn(repositoryRoot);
+		}
+
+		public MenuItem GetMenuItem(string repositoryRoot) {
+			return new MenuItem{Caption = "Push to..", Id = "advancedPusher"};
+		}
 	}
 }
