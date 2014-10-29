ccb63383b7df18e2e5da7d1bb19bd6da4b3d5639
Artem Smirnov <uluhonolulu@gmail.com>
2014-10-29 12:25:18 +0200

    New remote spike

--- src/Chpokk.Tests/Git/NewRemote.cs
+++ src/Chpokk.Tests/Git/NewRemote.cs
@@ -13,6 +13,7 @@ using Shouldly;
 namespace Chpokk.Tests.Git {
 	[TestFixture]
 	public class NewRemote : BaseCommandTest<GitRepositoryContext> {
+		public string DEFAULT_REMOTE = "origin";
 		[Test]
 		public void ShouldAddARemote() {
 			using (var repository = new Repository(Context.RepositoryRoot)) {
@@ -20,6 +21,18 @@ namespace Chpokk.Tests.Git {
 			}
 		}
 
-		public override void Act() {}
+		[Test]
+		public void NewRemoteShouldBecomeDefault() {
+			using (var repository = new Repository(Context.RepositoryRoot)) {
+				repository.Head.Remote.Name.ShouldBe(DEFAULT_REMOTE);
+			}			
+		}
+
+		public override void Act() {
+			using (var repository = new Repository(Context.RepositoryRoot)) {
+				repository.Network.Remotes.Add(DEFAULT_REMOTE, "https://ulu@appharbor.com/chpokk.git");
+				repository.Branches.Update(repository.Head, updater => updater.Remote = DEFAULT_REMOTE, updater => updater.UpstreamBranch = "refs/heads/master");
+			}
+		}
 	}
 }
