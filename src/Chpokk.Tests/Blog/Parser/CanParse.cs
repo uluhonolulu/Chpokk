34146bf468f391869e18d0f1fd611706b96aa6b5
Artem Smirnov <uluhonolulu@gmail.com>
2014-11-09 21:39:50 +0200

    Added Tanka

--- src/Chpokk.Tests/Blog/Parser/CanParse.cs
+++ src/Chpokk.Tests/Blog/Parser/CanParse.cs
@@ -1,27 +1,39 @@
-﻿using Arractas;
+﻿using System;
+using System.Web;
+using Arractas;
 using Chpokk.Tests.Infrastructure;
 using MbUnit.Framework;
+using Tanka.Markdown;
+using Tanka.Markdown.Html;
 
 namespace Chpokk.Tests.Blog.Parser {
 	[TestFixture]
 	public class CanParse: BaseQueryTest<SimpleConfiguredContext, BlogPostModel> {
 		[Test]
 		public void Test() {
-			//
-			// TODO: Add test logic here
-			//
+			Console.WriteLine(HttpUtility.HtmlEncode(Result.Content));
 		}
 
 		public override BlogPostModel Act() {
-			return Context.Container.Get<BlogPostParser>().Parse("");
+			return Context.Container.Get<BlogPostParser>().Parse("header  \n=");
 		}
 	}
 
 	public class BlogPostParser {
+		private readonly MarkdownParser _parser = new MarkdownParser(false);
+		//public BlogPostParser(MarkdownParser parser) {
+		//	_parser = parser;
+		//}
+
 		public BlogPostModel Parse(string source) {
-			return null;
+			var document = _parser.Parse(source);
+			var renderer = new MarkdownHtmlRenderer();
+			var content = renderer.Render(document);
+			return new BlogPostModel {Content = content};
 		}
 	}
 
-	public class BlogPostModel {}
+	public class BlogPostModel {
+		public string Content { get; set; }
+	}
 }
