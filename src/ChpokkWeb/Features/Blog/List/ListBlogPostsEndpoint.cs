36ce0aa656211afa5cbf47b86b777f4d3b474a30
Artem Smirnov <uluhonolulu@gmail.com>
2014-11-20 16:52:35 +0200

    Added a view for listing the posts

--- src/ChpokkWeb/Features/Blog/List/ListBlogPostsEndpoint.cs
+++ src/ChpokkWeb/Features/Blog/List/ListBlogPostsEndpoint.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Chpokk.Tests.Blog.Parser;
 using ChpokkWeb.Features.Blog.Post;
 using ChpokkWeb.Infrastructure;
 using FubuCore;
@@ -10,15 +11,17 @@ namespace ChpokkWeb.Features.Blog.List {
 	public class ListBlogPostsEndpoint {
 		private readonly IAppRootProvider _appRootProvider;
 		private readonly IFileSystem _fileSystem;
-		public ListBlogPostsEndpoint(IAppRootProvider appRootProvider, IFileSystem fileSystem) {
+		private readonly BlogPostParser _parser;
+		public ListBlogPostsEndpoint(IAppRootProvider appRootProvider, IFileSystem fileSystem, BlogPostParser parser) {
 			_appRootProvider = appRootProvider;
 			_fileSystem = fileSystem;
+			_parser = parser;
 		}
 
 		public ListBlogPostsModel DoIt() {
 			var blogRoot = _appRootProvider.AppRoot.AppendPath("Blog");
 			var postPaths = _fileSystem.FindFiles(blogRoot, FileSet.Shallow("*.md"));
-			var postModels = from path in postPaths select new BlogPostModel();
+			var postModels = from path in postPaths select _parser.LoadAndParse(path);
 			return new ListBlogPostsModel { Posts = postModels.ToArray() };
 		}
 	}
