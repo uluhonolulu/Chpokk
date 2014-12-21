1aa30ec6d5261c4b9074a9a20fac447a95b729bd
Artem Smirnov <uluhonolulu@gmail.com>
2014-12-21 22:19:19 +0200

    Setting up ReadMe

---
+++ src/ChpokkWeb/Features/ReadMe/ReadMeEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChpokkWeb.Features.Blog.Post;
+using FubuMVC.Core;
+
+namespace ChpokkWeb.Features.ReadMe {
+	public class ReadMeEndpoint {
+		public ReadMeModel DoIt(ReadMeInputModel model) {
+			return new ReadMeModel(){Content = "about me", Title = model.Name};
+		}
+	}
+
+	//[UrlPattern("/readme/{Name}")]
+	public class ReadMeInputModel {
+		[RouteInput]
+		public string Name { get; set; }
+	}
+
+	public class ReadMeModel: BlogPostModel {}
+}
