b28c803e5e1f140472bf9ce39f38e01924bc72fa
Artem Smirnov <uluhonolulu@gmail.com>
2015-02-03 13:04:30 +0300

    Moved the template installer code to production

--- src/Chpokk.Tests/Newing/TemplateInstaller.cs
+++ src/Chpokk.Tests/Newing/TemplateInstaller.cs
@@ -18,8 +18,6 @@ using Shouldly;
 namespace Chpokk.Tests.Newing {
 	[TestFixture]
 	public class TemplateInstaller: BaseCommandTest<RepositoryFolderContext> {
-		private FileSystem _fileSystem;
-		private TemplateTransformer _templateTransformer;
 		private const string CONTENT_FILENAME = "App.config";
 		private const string CONTENT_OTHERFILENAME = @"Properties\AssemblyInfo.cs";
 		private const string PROJECT_NAME = "NewProjectName";
@@ -50,43 +48,13 @@ namespace Chpokk.Tests.Newing {
 		}
 
 		public override void Act() {
-			//actually, let's use the code from ProjectCreator
 			var templateRelativePath = @"SystemFiles\Templates\ProjectTemplates\CSharp\Windows\1033\ConsoleApplication\csConsoleApplication.vstemplate";
+			var templatePath = Context.Container.Get<IAppRootProvider>().AppRoot.AppendPath(templateRelativePath);
 			var projectPath = Context.Container.Get<RepositoryManager>()
 			                            .NewGetAbsolutePathFor(Context.REPO_NAME, PROJECT_NAME.AppendPath(PROJECT_NAME + ".csproj"));
-			var templatePath = Context.Container.Get<IAppRootProvider>().AppRoot.AppendPath(templateRelativePath);
-			_fileSystem = Context.Container.Get<FileSystem>();
-			_templateTransformer = Context.Container.Get<TemplateTransformer>();
-			CreateProjectFromTemplate(projectPath, templatePath);
+			Context.Container.Get<ChpokkWeb.Features.ProjectManagement.TemplateInstaller>().CreateProjectFromTemplate(projectPath, templatePath);
 		}
 
-		public string CreateProjectFromTemplate(string projectPath, string templatePath) {
-			// from template
-			var projectFolder = projectPath.ParentDirectory();
-			var projectName = PROJECT_NAME;
-			var projectTemplateFolder = templatePath.ParentDirectory();
-			var replacements = new Dictionary<string, string>() { { "$safeprojectname$", projectName }, { "$targetframeworkversion$", "4.5" }, { "$guid1$", Guid.NewGuid().ToString() } };
-			var template = new Template(templatePath);
-			var projectItems = template.GetProjectItems();
-			foreach (var projectItem in projectItems) {
-				var templateFileRelativePath = projectItem.FileName;	//relative to template folder
-				var templateFileSourcePath = projectTemplateFolder.AppendPath(templateFileRelativePath);
-				var destinationRelativePath = projectItem.TargetFileName;
-				var destinationPath = projectFolder.AppendPath(destinationRelativePath);
-				var templateFileContent = _fileSystem.ReadStringFromFile(templateFileSourcePath);
-				var processedContent = _templateTransformer.Evaluate(templateFileContent, replacements);
-				_fileSystem.WriteStringToFile(destinationPath, processedContent);
-				
-			}
-
-			var projectFileSourceRelativePath = template.ProjectFileName;
-			var projectFileSourcePath = projectTemplateFolder.AppendPath(projectFileSourceRelativePath);
-			var destinationProjectPath = projectFolder.AppendPath(projectName + Path.GetExtension(projectFileSourceRelativePath));
-			var projectFileContent = _fileSystem.ReadStringFromFile(projectFileSourcePath);
-			var processedProjectContent = _templateTransformer.Evaluate(projectFileContent, replacements);
-			_fileSystem.WriteStringToFile(destinationProjectPath, processedProjectContent);
-			return null;
-		}
 
 		private string ProjectFolder {
 			get { return Context.RepositoryRoot.AppendPath(PROJECT_NAME); }
--- src/ChpokkWeb/Features/ProjectManagement/TemplateInstaller.cs
+++ src/ChpokkWeb/Features/ProjectManagement/TemplateInstaller.cs
@@ -1,9 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using FubuCore;
 
 namespace ChpokkWeb.Features.ProjectManagement {
 	public class TemplateInstaller {
+		private FileSystem _fileSystem;
+		private TemplateTransformer _templateTransformer;
+		public TemplateInstaller(FileSystem fileSystem, TemplateTransformer templateTransformer) {
+			_fileSystem = fileSystem;
+			_templateTransformer = templateTransformer;
+		}
+
+		public void CreateProjectFromTemplate(string projectPath, string templatePath) {
+			var projectFolder = projectPath.ParentDirectory();
+			var projectName = Path.GetFileNameWithoutExtension(projectPath);
+			var projectTemplateFolder = templatePath.ParentDirectory();
+			var replacements = new Dictionary<string, string>() { { "$safeprojectname$", projectName }, { "$targetframeworkversion$", "4.5" }, { "$guid1$", Guid.NewGuid().ToString() } };
+			var template = new Template(templatePath);
+			var projectItems = template.GetProjectItems();
+			foreach (var projectItem in projectItems) {
+				var templateFileRelativePath = projectItem.FileName;	//relative to template folder
+				var templateFileSourcePath = projectTemplateFolder.AppendPath(templateFileRelativePath);
+				var destinationRelativePath = projectItem.TargetFileName;
+				var destinationPath = projectFolder.AppendPath(destinationRelativePath);
+				var templateFileContent = _fileSystem.ReadStringFromFile(templateFileSourcePath);
+				var processedContent = _templateTransformer.Evaluate(templateFileContent, replacements);
+				_fileSystem.WriteStringToFile(destinationPath, processedContent);
+
+			}
+
+			var projectFileSourceRelativePath = template.ProjectFileName;
+			var projectFileSourcePath = projectTemplateFolder.AppendPath(projectFileSourceRelativePath);
+			var projectFileContent = _fileSystem.ReadStringFromFile(projectFileSourcePath);
+			var processedProjectContent = _templateTransformer.Evaluate(projectFileContent, replacements);
+			_fileSystem.WriteStringToFile(projectPath, processedProjectContent);
+		}
 	}
 }
