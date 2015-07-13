28694267a37faea4b634839cebd65c01d1991e2f
Artem Smirnov <uluhonolulu@gmail.com>
2015-07-13 13:41:58 +0300

    Added Web Publishing menu button

---
+++ src/ChpokkWeb/Features/Editor/Menu/Policies/WebPublishEditorMenuPolicy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using FubuMVC.Navigation;
+
+namespace ChpokkWeb.Features.Editor.Menu.Policies {
+	public class WebPublishEditorMenuPolicy :IEditorMenuPolicy{
+		public bool Matches(string repositoryPath) {
+			var webConfigPaths = Directory.EnumerateFiles(repositoryPath, "web.config", SearchOption.AllDirectories);
+			return webConfigPaths.Any();
+		}
+
+		public IEnumerable<MenuItemToken> GetMenuItems() {
+			yield return new MenuItemToken() { Text = "Preview your Web", Key = "publishWeb", MenuItemState = MenuItemState.Available };
+		}
+	}
+}
