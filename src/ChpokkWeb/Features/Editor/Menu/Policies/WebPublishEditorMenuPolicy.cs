6f2cde83f15570711ce86d002c57a80c7cb0a246
Artem Smirnov <uluhonolulu@gmail.com>
2015-07-19 12:31:35 +0300

    UI for configuring Web publishing

--- src/ChpokkWeb/Features/Editor/Menu/Policies/WebPublishEditorMenuPolicy.cs
+++ src/ChpokkWeb/Features/Editor/Menu/Policies/WebPublishEditorMenuPolicy.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using FubuCore;
 using FubuMVC.Navigation;
+using LibGit2Sharp;
 
 namespace ChpokkWeb.Features.Editor.Menu.Policies {
 	public class WebPublishEditorMenuPolicy :IEditorMenuPolicy{
 		public bool Matches(string repositoryPath) {
+			return RepositoryHasWebs(repositoryPath) && !(IsGitRepository(repositoryPath) && HasGitRemotes(repositoryPath));
+		}
+
+		private bool RepositoryHasWebs(string repositoryPath) {
 			var webConfigPaths = Directory.EnumerateFiles(repositoryPath, "web.config", SearchOption.AllDirectories);
 			return webConfigPaths.Any();
+		}
+
+		private bool IsGitRepository(string repositoryPath) {
+			var path = repositoryPath.AppendPath(".git");
+			return Directory.Exists(path);
+		}
+
+		private bool HasGitRemotes(string repositoryPath) {
+			using (var repository = new Repository(repositoryPath)) {
+				return repository.Network.Remotes.Any();
+			}
 		}
 
 		public IEnumerable<MenuItemToken> GetMenuItems() {
