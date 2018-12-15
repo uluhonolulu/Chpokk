cff75c1658da6a4b96e17c6ff19d06f0d17533ab
Artem Smirnov <uluhonolulu@gmail.com>
2018-12-15 20:16:20 +0200

    Update CloningFromAssembla.cs

--- src/Chpokk.Tests/Spikes/CloningFromAssembla.cs
+++ src/Chpokk.Tests/Spikes/CloningFromAssembla.cs
@@ -12,41 +12,5 @@ using MbUnit.Framework.ContractVerifiers;
 namespace Chpokk.Tests.Spikes {
 	[TestFixture]
 	public class CloningFromAssembla {
-		[Test]
-		public void CanCloneSuccessfully() {
-			var url = "https://rootgr1dorg@git.assembla.com/gr1dorg.git";
-			var userName = "rootgr1dorg";
-			var password = "5.25InchDisk ";
-			url = "https://git.assembla.com/chpokk-private.git";//https://@git.assembla.com/.git - See more at: http://blog.assembla.com/assemblablog/tabid/12618/bid/70667/Git-over-HTTP.aspx#sthash.H34TZqNQ.dpuf
-			userName = "uluhonolulu";
-			//password = "xd11SvG23";
-			//url = "https://uluhonolulu@bitbucket.org/uluhonolulu/chpokk-test.git";
-			password = "p4SSw0RD";
-			var targetFolder = AppDomain.CurrentDomain.SetupInformation.ApplicationBase.ParentDirectory().AppendPath(@"Users\ulu\Repositories\private");
-			var cloneOptions = new CloneOptions
-				{
-					CredentialsProvider = (s, fromUrl, types) => new UsernamePasswordCredentials() {Username = userName, Password = password}
-				};
-			Repository.Clone(url, targetFolder, cloneOptions);
-		}
-
-		[Test]
-		public void CanCloneFromBitbucket() {
-			var url = "https://uluhonolulu@bitbucket.org/uluhonolulu/chpokk-test.git";
-			var userName = "uluhonolulu";
-			var password = "xd11SvG23";
-			var targetFolder = @"D:\Projects\Chpokk\src\ChpokkWeb\UserFiles\ulu\private";
-			DirectoryHelper.DeleteDirectory(targetFolder);
-			var cloneOptions = new CloneOptions {
-				CredentialsProvider = (s, fromUrl, types) => new UsernamePasswordCredentials() { Username = userName, Password = password }
-			};
-			try {
-				Repository.Clone(url, targetFolder, cloneOptions);
-			}
-			finally {
-				DirectoryHelper.DeleteDirectory(targetFolder);	
-			}
-			
-		}
 	}
 }
